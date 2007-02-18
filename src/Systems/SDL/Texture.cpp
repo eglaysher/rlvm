1226ce09f3d91ceaf8e3b9746aa4ce24fadc4a7f
Elliot Glaysher <glaysher@umich.edu>
2007-02-18 02:53:22 +0000

    Split the Texture class into its own source file.

---
+++ src/Systems/SDL/Texture.cpp
@@ -0,0 +1,336 @@
+// This file is part of RLVM, a RealLive virutal machine clone.
+//
+// -----------------------------------------------------------------------
+//
+// Copyright (C) 2006 El Riot
+//  
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//  
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//  
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//  
+// -----------------------------------------------------------------------
+
+#include "Texture.hpp"
+
+#include <boost/bind.hpp>
+#include <SDL/SDL.h>
+#include <SDL/SDL_opengl.h>
+
+#include <iostream>
+#include <cstdio>
+
+#include "Systems/Base/GraphicsObject.hpp"
+#include "Systems/SDL/SDLGraphicsSystem.hpp"
+
+#include "libReallive/defs.h"
+
+using namespace std;
+using namespace boost;
+using namespace libReallive;
+
+namespace {
+
+void ShowGLErrors(void)
+{
+  GLenum error;
+  const GLubyte* errStr;
+  if ((error = glGetError()) != GL_NO_ERROR)
+  {
+    errStr = gluErrorString(error);
+    fprintf(stderr, "OpenGL Error: %s\n", errStr);
+    abort();
+  }
+}
+
+// -----------------------------------------------------------------------
+
+int SafeSize(int i) {
+  static GLint maxTextureSize = 0;
+  if(maxTextureSize == 0)
+    glGetIntegerv(GL_MAX_TEXTURE_SIZE, &maxTextureSize);
+  int p;
+
+  if (i > maxTextureSize) return maxTextureSize;
+
+  for (p = 0; p < 24; p++)
+    if (i <= (1<<p))
+      return 1<<p;
+
+  return maxTextureSize;
+}
+}
+
+Texture::Texture(SDL_Surface* surface)
+  : m_logicalWidth(surface->w), m_logicalHeight(surface->h)
+{
+  glGenTextures(1, &m_textureID);
+  glBindTexture(GL_TEXTURE_2D, m_textureID);
+  ShowGLErrors();
+//  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+//  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  SDL_LockSurface(surface);
+
+  Uint8 bytesPerPixel = surface->format->BytesPerPixel;
+  GLint byteOrder = GL_RGBA;
+  GLint byteType = GL_UNSIGNED_BYTE;
+
+  // Determine the byte order of the surface
+  SDL_PixelFormat* format = surface->format;
+  if(bytesPerPixel == 4)
+  {
+    // If the order is RGBA...
+    if(format->Rmask == 0xFF000000 && format->Amask == 0xFF)
+      byteOrder = GL_RGBA;
+    // OSX's crazy ARGB pixel format
+    else if(format->Amask == 0xFF000000 && format->Rmask == 0xFF0000 &&
+            format->Gmask == 0xFF00 && format->Bmask == 0xFF)
+    {
+      // This is an insane hack to get around OSX's crazy byte order
+      // for alpha on PowerPC. Since there isn't a GL_ARGB type, we
+      // need to specify BGRA and then tell the byte type to be
+      // reversed order.
+      byteOrder = GL_BGRA;
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+      byteType = GL_UNSIGNED_INT_8_8_8_8_REV;
+#else
+      byteType = GL_UNSIGNED_INT_8_8_8_8;
+#endif
+    }
+    else
+    {
+      cerr << "Unknown mask! It's not Hakuro!" << endl;
+    }
+  }
+  else if(bytesPerPixel == 3)
+  {
+    // For now, just assume RGB.
+    byteOrder = GL_RGB;
+    cerr << "Warning: Am I really an RGB Surface? Check Texture::Texture()!"
+         << endl;
+  }
+  else
+    throw Error("Error loading texture: bytesPerPixel != 3 or 4. Duuudee...");
+
+  // I have no idea what I'm doing!
+//    cerr << "MASK: [" << hex << surface->format->Rmask 
+//         << ", " << surface->format->Gmask 
+//         << ", " << surface->format->Bmask 
+//         << ", " << surface->format->Amask 
+//         << "]" << endl;
+
+  m_textureWidth = SafeSize(surface->w);
+  m_textureHeight = SafeSize(surface->h);
+  glTexImage2D(GL_TEXTURE_2D, 0, bytesPerPixel,
+               m_textureWidth, m_textureHeight,
+               0, 
+               byteOrder,
+               byteType, NULL);
+  ShowGLErrors();
+
+  // Check the surface for the byte order of the surface:
+//   cerr << "Size: {" << dec << surface->w << ", " << surface->h << "}"
+//        << " First pixel: " << hex << ((int*)surface->pixels)[0] << endl;
+
+  glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, surface->w, surface->h,
+                  byteOrder, byteType, surface->pixels);            
+  ShowGLErrors();
+
+  SDL_UnlockSurface(surface);
+}
+
+// -----------------------------------------------------------------------
+
+Texture::~Texture()
+{
+  glDeleteTextures(1, &m_textureID);
+//  cerr << "Deleteing texture with texid " << m_textureID << endl;
+  ShowGLErrors();
+}
+
+// -----------------------------------------------------------------------
+
+// This is really broken and brain dead.
+void Texture::renderToScreen(int x1, int y1, int x2, int y2,
+                             int dx1, int dy1, int dx2, int dy2,
+                             int opacity)
+{
+  // For the time being, we are dumb and assume that it's one texture
+  
+  float thisx1 = float(x1) / m_textureWidth;
+  float thisy1 = float(y1) / m_textureHeight;
+  float thisx2 = float(x2) / m_textureWidth;
+  float thisy2 = float(y2) / m_textureHeight;
+
+  glBindTexture(GL_TEXTURE_2D, m_textureID);
+
+  // Blend when we have less opacity
+//  if(opacity < 255)
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+  glBegin(GL_QUADS);
+  {
+    glColor4ub(255, 255, 255, opacity);
+    glTexCoord2f(thisx1, thisy1);
+    glVertex2i(dx1, dy1);
+    glTexCoord2f(thisx2, thisy1);
+    glVertex2i(dx2, dy1);
+    glTexCoord2f(thisx2, thisy2);
+    glVertex2i(dx2, dy2);        
+    glTexCoord2f(thisx1, thisy2);
+    glVertex2i(dx1, dy2);
+  }
+  glEnd();
+  glBlendFunc(GL_ONE, GL_ZERO);
+}
+
+// -----------------------------------------------------------------------
+
+void Texture::renderToScreen(int x1, int y1, int x2, int y2,
+                             int dx1, int dy1, int dx2, int dy2,
+                             const int opacity[4])
+{
+    // For the time being, we are dumb and assume that it's one texture
+  
+  float thisx1 = float(x1) / m_textureWidth;
+  float thisy1 = float(y1) / m_textureHeight;
+  float thisx2 = float(x2) / m_textureWidth;
+  float thisy2 = float(y2) / m_textureHeight;
+
+  glBindTexture(GL_TEXTURE_2D, m_textureID);
+
+  // Blend when we have less opacity
+  if(find_if(opacity, opacity + 4, bind(std::less<int>(), _1, 255)) 
+     != opacity + 4)
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+  glBegin(GL_QUADS);
+  {
+    glColor4ub(255, 255, 255, opacity[0]);
+    glTexCoord2f(thisx1, thisy1);
+    glVertex2i(dx1, dy1);
+    glColor4ub(255, 255, 255, opacity[1]);
+    glTexCoord2f(thisx2, thisy1);
+    glVertex2i(dx2, dy1);
+    glColor4ub(255, 255, 255, opacity[2]);
+    glTexCoord2f(thisx2, thisy2);
+    glVertex2i(dx2, dy2);        
+    glColor4ub(255, 255, 255, opacity[3]);
+    glTexCoord2f(thisx1, thisy2);
+    glVertex2i(dx1, dy2);
+  }
+  glEnd();
+  glBlendFunc(GL_ONE, GL_ZERO);
+}
+
+// -----------------------------------------------------------------------
+
+void Texture::renderToScreenAsObject(const GraphicsObject& go, SDLSurface& surface)
+{
+  // Figure out the source to clip out of the image
+  int pattNo = go.pattNo();
+  int xSrc1 = surface.getPattern(pattNo).x1;
+  int ySrc1 = surface.getPattern(pattNo).y1;
+  int xSrc2 = surface.getPattern(pattNo).x2;
+  int ySrc2 = surface.getPattern(pattNo).y2;
+
+  // Figure out position to display on
+  int xPos1 = go.x() + go.xAdjustmentSum();
+  int yPos1 = go.y() + go.yAdjustmentSum();
+  int xPos2 = xPos1 + (xSrc2 - xSrc1) * (go.width() / 100.0f);
+  int yPos2 = yPos1 + (ySrc2 - ySrc1) * (go.height() / 100.0f);
+
+  // Convert the pixel coordinates into [0,1) texture coordinates
+  float thisx1 = float(xSrc1) / m_textureWidth;
+  float thisy1 = float(ySrc1) / m_textureHeight;
+  float thisx2 = float(xSrc2) / m_textureWidth;
+  float thisy2 = float(ySrc2) / m_textureHeight;
+
+//   cerr << "patt: " << pattNo << ", texid: " << m_textureID << ", alpha: " << go.alpha()
+//        << ", src:{" << xSrc1 << "," << ySrc1 << "," << xSrc2 << "," << ySrc2 << "}"
+//        << ", dst:{" << xPos1 << "," << yPos1 << "," << xPos2 << "," << yPos2 << "}"
+//        << endl;
+
+  glBindTexture(GL_TEXTURE_2D, m_textureID);
+
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+  glPushMatrix();
+  {
+    // Move the "origin" to the correct position.
+    glTranslatef(go.xOrigin(), go.yOrigin(), 0);
+
+    // Rotate here?
+    glRotatef(float(go.rotation()) / 10, 0, 0, 1);
+
+//    cerr << "Color: " << go.tintR() << ", " << go.tintG() << ", " << go.tintB()
+//         << ", " << go.alpha() << endl;
+
+    glBegin(GL_QUADS);
+    {
+      glColor4ub(go.tintR(), go.tintG(), go.tintB(), go.alpha());
+      glTexCoord2f(thisx1, thisy1);
+      glVertex2i(xPos1, yPos1);
+      glTexCoord2f(thisx2, thisy1);
+      glVertex2i(xPos2, yPos1);
+      glTexCoord2f(thisx2, thisy2);
+      glVertex2i(xPos2, yPos2);        
+      glTexCoord2f(thisx1, thisy2);
+      glVertex2i(xPos1, yPos2);
+    }
+    glEnd();
+
+    glBlendFunc(GL_ONE, GL_ZERO);
+  }
+  glPopMatrix();
+
+  ShowGLErrors();
+}
+
+// -----------------------------------------------------------------------
+
+void Texture::rawRenderQuad(const int srcCoords[8], 
+                            const int destCoords[8],
+                            const int opacity[4])
+{
+  // For the time being, we are dumb and assume that it's one texture
+  float textureCoords[8];
+  for(int i = 0; i < 8; i += 2)
+  {
+    textureCoords[i] = float(srcCoords[i]) / m_textureWidth;
+    textureCoords[i + 1] = float(srcCoords[i + 1]) / m_textureHeight;
+  }
+
+  glBindTexture(GL_TEXTURE_2D, m_textureID);
+
+  // Blend when we have less opacity
+  if(find_if(opacity, opacity + 4, bind(std::less<int>(), _1, 255)) 
+     != opacity + 4)
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+  glBegin(GL_QUADS);
+  {
+    for(int i = 0; i < 4; i++)
+    {
+      glColor4ub(255, 255, 255, opacity[i]);
+
+      int firstIndex = i * 2;
+      int secondIndex = firstIndex + 1;
+      glTexCoord2f(srcCoords[firstIndex], srcCoords[secondIndex]);
+      glVertex2i(destCoords[firstIndex], destCoords[secondIndex]);
+    }
+  }
+  glEnd();
+  glBlendFunc(GL_ONE, GL_ZERO);
+}
---
+++ src/Systems/SDL/Texture.hpp
@@ -0,0 +1,74 @@
+// This file is part of RLVM, a RealLive virutal machine clone.
+//
+// -----------------------------------------------------------------------
+//
+// Copyright (C) 2006 El Riot
+//  
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//  
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//  
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//  
+// -----------------------------------------------------------------------
+
+/**
+ * @file   Texture.hpp
+ * @author Elliot Glaysher
+ * @date   Sat Feb 17 20:21:59 2007
+ * 
+ * @brief  Contains an OpenGL texture and some metadata.
+ */
+
+#ifndef __Texture_hpp__
+#define __Texture_hpp__
+
+#include <SDL/SDL_opengl.h>
+
+struct SDL_Surface;
+class SDLSurface;
+class GraphicsObject;
+
+class Texture
+{
+private:
+  unsigned int m_logicalWidth;
+  unsigned int m_logicalHeight;
+
+  unsigned int m_textureWidth;
+  unsigned int m_textureHeight;
+
+  GLuint m_textureID;
+
+public:
+  Texture(SDL_Surface* surface);
+  ~Texture();
+
+  int width() { return m_logicalWidth; }
+  int height() { return m_logicalHeight; }
+  GLuint textureId() { return m_textureID; }
+
+  void renderToScreenAsObject(const GraphicsObject& go, SDLSurface& surface);
+
+  void renderToScreen(int x1, int y1, int x2, int y2,
+                      int dx1, int dy1, int dx2, int dy2,
+                      int opacity);
+  void renderToScreen(int x1, int y1, int x2, int y2,
+                      int dx1, int dy1, int dx2, int dy2,
+                      const int opacity[4]);
+
+  void rawRenderQuad(const int srcCoords[8], 
+                     const int destCoords[8],
+                     const int opacity[4]);
+
+};
+
+#endif
