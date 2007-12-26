a6780405a87dddcda63a2522f461d8f99e69d532
Elliot Glaysher <glaysher@umich.edu>
2007-12-26 14:23:31 -0500

    Start moving empty function definitions out of Surface and into their own cpp file.
    P.S.: The Surface interface needs to be documented.

--- src/Systems/Base/Surface.hpp
+++ src/Systems/Base/Surface.hpp
@@ -1,3 +1,30 @@
+// -*- Mode: C++; tab-width:2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi:tw=80:et:ts=2:sts=2
+//
+// -----------------------------------------------------------------------
+//
+// This file is part of RLVM, a RealLive virtual machine clone.
+//
+// -----------------------------------------------------------------------
+//
+// Copyright (C) 2007 Elliot Glaysher
+//  
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//  
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//  
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//  
+// -----------------------------------------------------------------------
+
 #ifndef __Surface_hpp__
 #define __Surface_hpp__
 
@@ -5,9 +32,7 @@ class GraphicsObject;
 struct GraphicsObjectOverride;
 
 /** 
- * Abstract concept of a surface. Used 
- * 
- * @return 
+ * Abstract concept of a surface.
  */
 class Surface
 {
@@ -19,7 +44,8 @@ public:
   };
 
 public:
-  virtual ~Surface() { }
+  Surface();
+  virtual ~Surface();
 
   virtual int width() const = 0;
   virtual int height() const = 0;
