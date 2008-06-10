3d342c02927f2b6d17677b23fb2b9674dcca2b86
Elliot Glaysher <glaysher@umich.edu>
2008-06-09 19:10:36 -0700

    Forgot to relicense some files under GPLv3. Change the license text.

--- src/Systems/SDL/SDLSoundChunk.cpp
+++ src/Systems/SDL/SDLSoundChunk.cpp
@@ -11,7 +11,7 @@
 //  
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
+// the Free Software Foundation; either version 3 of the License, or
 // (at your option) any later version.
 //  
 // This program is distributed in the hope that it will be useful,
@@ -23,6 +23,10 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 //  
+// -----------------------------------------------------------------------
+
+#include "Precompiled.hpp"
+
 // -----------------------------------------------------------------------
 
 #include "Systems/Base/SoundSystem.hpp"
