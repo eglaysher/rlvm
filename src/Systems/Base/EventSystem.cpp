eglaysher/rlvm

commit 85b44fb2750b6bf3a880d6136190fa5da0b3e06f

Rework how Event system dispatches events, along with general cleanups.
I'm doing this so a passive TextSystem can listen to mouse clicks and
key presses, but only capture them in certain cases (and then these
clicks wouldn't be passed on to whatever EventHandler is currently
working...)

It also might be my imagination, but CLANNAD's cursor seems to be
smoother now...

This is a large change to a core system; in case of regression, look
around here.

src/Systems/Base/EventSystem.cpp
@@ -91,29 +91,30 @@ void EventSystem::removeEventHandler(EventHandler* handler)
 
 // -----------------------------------------------------------------------
 
-void EventSystem::addMouseListener(MouseListener* listener)
+void EventSystem::addMouseListener(EventListener* listener)
 {
-  MouseListeners::iterator it =
-    std::find(mouse_listeners_.begin(), mouse_listeners_.end(), listener);
+  EventListeners::iterator it =
+    std::find(event_listeners_.begin(), event_listeners_.end(), listener);
 
-  if(it == mouse_listeners_.end())
-    mouse_listeners_.push_back(listener);
+  if(it == event_listeners_.end())
+    event_listeners_.push_back(listener);
 }
 
 // -----------------------------------------------------------------------
 
-void EventSystem::removeMouseListener(MouseListener* listener)
+void EventSystem::removeMouseListener(EventListener* listener)
 {
-  MouseListeners::iterator it =
-    std::find(mouse_listeners_.begin(), mouse_listeners_.end(), listener);
+  EventListeners::iterator it =
+    std::find(event_listeners_.begin(), event_listeners_.end(), listener);
 
-  if(it != mouse_listeners_.end())
-    mouse_listeners_.erase(it);
+  if(it != event_listeners_.end())
+    event_listeners_.erase(it);
 }
 
 // -----------------------------------------------------------------------
 
-void EventSystem::setFrameCounter(int layer, int frame_counter, FrameCounter* counter)
+void EventSystem::setFrameCounter(int layer, int frame_counter,
+                                  FrameCounter* counter)
 {
   checkLayerAndCounter(layer, frame_counter);
   frame_counters_[layer][frame_counter].reset(counter);

src/Systems/Base/EventSystem.hpp
@@ -40,7 +40,7 @@ class RLMachine;
 class Gameexe;
 class FrameCounter;
 class EventHandler;
-class MouseListener;
+class EventListener;
 
 // -----------------------------------------------------------------------
 
@@ -133,8 +133,8 @@ public:
   virtual void addEventHandler(EventHandler* handler);
   virtual void removeEventHandler(EventHandler* handler);
 
-  virtual void addMouseListener(MouseListener* listener);
-  virtual void removeMouseListener(MouseListener* listener);
+  virtual void addMouseListener(EventListener* listener);
+  virtual void removeMouseListener(EventListener* listener);
   /// @}
 
   /**
@@ -213,13 +213,13 @@ public:
 
 protected:
   typedef std::vector<EventHandler*> Handlers;
-  typedef std::vector<MouseListener*> MouseListeners;
+  typedef std::vector<EventListener*> EventListeners;
 
   Handlers::iterator handlers_begin() { return event_handlers_.begin(); }
   Handlers::iterator handlers_end() { return event_handlers_.end(); }
 
-  MouseListeners::iterator listeners_begin() { return mouse_listeners_.begin(); }
-  MouseListeners::iterator listeners_end() { return mouse_listeners_.end(); }
+  EventListeners::iterator listeners_begin() { return event_listeners_.begin(); }
+  EventListeners::iterator listeners_end() { return event_listeners_.end(); }
 
 private:
   boost::scoped_ptr<FrameCounter> frame_counters_[255][2];
@@ -229,7 +229,7 @@ private:
   void checkLayerAndCounter(int layer, int counter);
 
   Handlers event_handlers_;
-  MouseListeners mouse_listeners_;
+  EventListeners event_listeners_;
 
   EventSystemGlobals globals_;
 };

src/Modules/Module_Sel.cpp
@@ -118,7 +118,7 @@ void Sel_LongOperation::mouseMotion(const Point& pos)
 
 // -----------------------------------------------------------------------
 
-void Sel_LongOperation::mouseButtonStateChanged(MouseButton mouseButton,
+bool Sel_LongOperation::mouseButtonStateChanged(MouseButton mouseButton,
                                                 bool pressed)
 {
   EventSystem& es = machine_.system().event();
@@ -129,6 +129,7 @@ void Sel_LongOperation::mouseButtonStateChanged(MouseButton mouseButton,
   {
     Point pos = es.getCursorPos();
     machine_.system().text().handleMouseClick(machine_, pos, pressed);
+    return true;
     break;
   }
   case MOUSE_RIGHT:
@@ -137,6 +138,8 @@ void Sel_LongOperation::mouseButtonStateChanged(MouseButton mouseButton,
   default:
 	break;
   }
+
+  return false;
 }
 
 // -----------------------------------------------------------------------

src/Modules/Module_Sel.hpp
@@ -67,7 +67,7 @@ public:
   virtual bool operator()(RLMachine& machine);
 
   virtual void mouseMotion(const Point&);
-  virtual void mouseButtonStateChanged(MouseButton mouseButton, bool pressed);
+  virtual bool mouseButtonStateChanged(MouseButton mouseButton, bool pressed);
 };
 
 // -----------------------------------------------------------------------

src/Modules/Module_Sys.cpp
@@ -112,22 +112,30 @@ struct LongOp_wait : public LongOperation, public EventHandler
   /**
    * Listen for mouseclicks (provided by EventHandler).
    */
-  void mouseButtonStateChanged(MouseButton mouseButton, bool pressed)
+  bool mouseButtonStateChanged(MouseButton mouseButton, bool pressed)
   {
-    if(pressed && break_on_clicks_)
-    {
-      if(mouseButton == MOUSE_LEFT)
+    if (pressed && break_on_clicks_) {
+      if (mouseButton == MOUSE_LEFT) {
         button_pressed_ = 1;
-      else if(mouseButton == MOUSE_RIGHT)
+        return true;
+      } else if(mouseButton == MOUSE_RIGHT) {
         button_pressed_ = -1;
+        return true;
+      }
     }
+
+    return false;
   }
 
-  void keyStateChanged(KeyCode keyCode, bool pressed)
+  bool keyStateChanged(KeyCode keyCode, bool pressed)
   {
-    if(pressed && break_on_ctrl_pressed_ &&
-       (keyCode == RLKEY_RCTRL || keyCode == RLKEY_LCTRL))
+    if (pressed && break_on_ctrl_pressed_ &&
+        (keyCode == RLKEY_RCTRL || keyCode == RLKEY_LCTRL)) {
       ctrl_pressed_ = true;
+      return true;
+    }
+
+    return false;
   }
 
   bool operator()(RLMachine& machine)
@@ -135,21 +143,18 @@ struct LongOp_wait : public LongOperation, public EventHandler
     bool done = machine.system().event().getTicks() > target_time_ ||
       ctrl_pressed_;
 
-    if(mouse_moved_)
-    {
+    if (mouse_moved_) {
       machine.system().graphics().markScreenAsDirty(GUT_MOUSE_MOTION);
       mouse_moved_ = false;
     }
 
-    if(break_on_clicks_)
-    {
-      if(button_pressed_)
-      {
+    if (break_on_clicks_) {
+      if (button_pressed_) {
         done = true;
         machine.setStoreRegister(button_pressed_);
-      }
-      else if(done)
+      } else if(done) {
         machine.setStoreRegister(0);
+      }
     }
 
     return done;

src/Modules/Module_Sys_Timer.cpp
@@ -91,15 +91,19 @@ struct LongOp_time : public LongOperation, public EventHandler
   /**
    * Listen for mouseclicks (provided by EventHandler).
    */
-  void mouseButtonStateChanged(MouseButton mouseButton, bool pressed)
+  bool mouseButtonStateChanged(MouseButton mouseButton, bool pressed)
   {
-    if(pressed)
-    {
-      if(mouseButton == MOUSE_LEFT)
+    if (pressed) {
+      if (mouseButton == MOUSE_LEFT) {
         button_pressed_ = 1;
-      else if(mouseButton == MOUSE_RIGHT)
+        return true;
+      } else if(mouseButton == MOUSE_RIGHT) {
         button_pressed_ = -1;
+        return true;
+      }
     }
+
+    return false;
   }
 
   bool operator()(RLMachine& machine)
@@ -107,27 +111,24 @@ struct LongOp_time : public LongOperation, public EventHandler
     EventSystem& es = machine.system().event();
     bool done = false;
 
-    if(mouse_moved_)
-    {
+    if (mouse_moved_) {
       machine.system().graphics().markScreenAsDirty(GUT_MOUSE_MOTION);
       mouse_moved_ = false;
     }
 
     // First check to see if we're done because of time.
-    if(es.getTimer(layer_, counter_).read(es) > target_time_)
+    if (es.getTimer(layer_, counter_).read(es) > target_time_)
       done = true;
 
-    if(cancel_on_click_)
-    {
+    if (cancel_on_click_) {
       // The underlying timeC returns a value. Manually set that
       // value here.
-      if(button_pressed_)
-      {
+      if (button_pressed_) {
         done = true;
         machine.setStoreRegister(button_pressed_);
-      }
-      else if(done)
+      } else if(done) {
         machine.setStoreRegister(0);
+      }
     }
 
     return done;

src/Modules/PauseLongOperation.cpp
@@ -105,7 +105,7 @@ void PauseLongOperation::mouseMotion(const Point& p)
 
 // -----------------------------------------------------------------------
 
-void PauseLongOperation::mouseButtonStateChanged(MouseButton mouseButton,
+bool PauseLongOperation::mouseButtonStateChanged(MouseButton mouseButton,
                                                  bool pressed)
 {
   GraphicsSystem& graphics = machine.system().graphics();
@@ -121,8 +121,10 @@ void PauseLongOperation::mouseButtonStateChanged(MouseButton mouseButton,
     // Only unhide the interface on release of the left mouse button
     if(graphics.interfaceHidden())
     {
-      if(!pressed)
+      if(!pressed) {
         graphics.toggleInterfaceHidden();
+        return true;
+      }
     }
     else if(!machine.system().text().handleMouseClick(machine, pos, pressed))
     {
@@ -137,59 +139,73 @@ void PauseLongOperation::mouseButtonStateChanged(MouseButton mouseButton,
         {
           is_done_ = true;
         }
+
+        return true;
       }
     }
     break;
   }
   case MOUSE_RIGHT:
-    if(pressed)
+    if (pressed) {
       handleSyscomCall();
+      return true;
+    }
     break;
   case MOUSE_WHEELUP:
-    if(pressed)
+    if (pressed) {
       text.backPage(machine);
+      return true;
+    }
     break;
   case MOUSE_WHEELDOWN:
-    if(pressed)
+    if (pressed) {
       text.forwardPage(machine);
+      return true;
+    }
     break;
   default:
     break;
   }
+
+  return false;
 }
 
 // -----------------------------------------------------------------------
 
-void PauseLongOperation::keyStateChanged(KeyCode keyCode, bool pressed)
+bool PauseLongOperation::keyStateChanged(KeyCode keyCode, bool pressed)
 {
   GraphicsSystem& graphics = machine.system().graphics();
   TextSystem& text = machine.system().text();
+  bool handled = false;
 
-  if(pressed)
-  {
-    if(graphics.interfaceHidden())
-    {
+  if(pressed) {
+    if(graphics.interfaceHidden()) {
       graphics.toggleInterfaceHidden();
-    }
-    else
-    {
+      handled = true;
+    } else {
       bool ctrlKeySkips = text.ctrlKeySkip();
 
       if(ctrlKeySkips &&
-         (keyCode == RLKEY_RCTRL || keyCode == RLKEY_LCTRL))
-      {
+         (keyCode == RLKEY_RCTRL || keyCode == RLKEY_LCTRL)) {
         is_done_ = true;
-      }
-      else if(keyCode == RLKEY_SPACE)
+        handled = true;
+      } else if(keyCode == RLKEY_SPACE) {
         graphics.toggleInterfaceHidden();
-      else if(keyCode == RLKEY_UP)
+        handled = true;
+      } else if(keyCode == RLKEY_UP) {
         text.backPage(machine);
-      else if(keyCode == RLKEY_DOWN)
+        handled = true;
+      } else if(keyCode == RLKEY_DOWN) {
         text.forwardPage(machine);
-      else if(keyCode == RLKEY_RETURN)
+        handled = true;
+      } else if(keyCode == RLKEY_RETURN) {
         is_done_ = true;
+        handled = true;
+      }
     }
   }
+
+  return handled;
 }
 
 // -----------------------------------------------------------------------

src/Modules/PauseLongOperation.hpp
@@ -63,8 +63,8 @@ public:
 
   // ------------------------------------------ [ EventHandler interface ]
   void mouseMotion(const Point&);
-  void mouseButtonStateChanged(MouseButton mouseButton, bool pressed);
-  void keyStateChanged(KeyCode keyCode, bool pressed);
+  bool mouseButtonStateChanged(MouseButton mouseButton, bool pressed);
+  bool keyStateChanged(KeyCode keyCode, bool pressed);
 
   // ----------------------------------------- [ LongOperation interface ]
   bool operator()(RLMachine& machine);

commit 9a76a6c80fb94f0556f6308c93330fbb65ff7bf9

Fix broken save/load buttons in Planetarian.
Very weird regression introduced in 85b44fb2750b6bf3a880d6136190fa5da0b3e06f
which broke the Planetarian menus. Somehow, it seems like there can sometimes
be duplicates in these lists because of something related to loose/gain
focus...

src/Systems/Base/EventSystem.cpp
@@ -71,44 +71,28 @@ EventSystem::~EventSystem()
 
 void EventSystem::addEventHandler(EventHandler* handler)
 {
-  Handlers::iterator it =
-    std::find(event_handlers_.begin(), event_handlers_.end(), handler);
-
-  if(it == event_handlers_.end())
-    event_handlers_.push_back(handler);
+  event_handlers_.insert(handler);
 }
 
 // -----------------------------------------------------------------------
 
 void EventSystem::removeEventHandler(EventHandler* handler)
 {
-  Handlers::iterator it =
-    std::find(event_handlers_.begin(), event_handlers_.end(), handler);
-
-  if(it != event_handlers_.end())
-    event_handlers_.erase(it);
+  event_handlers_.erase(handler);
 }
 
 // -----------------------------------------------------------------------
 
 void EventSystem::addMouseListener(EventListener* listener)
 {
-  EventListeners::iterator it =
-    std::find(event_listeners_.begin(), event_listeners_.end(), listener);
-
-  if(it == event_listeners_.end())
-    event_listeners_.push_back(listener);
+  event_listeners_.insert(listener);
 }
 
 // -----------------------------------------------------------------------
 
 void EventSystem::removeMouseListener(EventListener* listener)
 {
-  EventListeners::iterator it =
-    std::find(event_listeners_.begin(), event_listeners_.end(), listener);
-
-  if(it != event_listeners_.end())
-    event_listeners_.erase(it);
+  event_listeners_.erase(listener);
 }
 
 // -----------------------------------------------------------------------

src/Systems/Base/EventSystem.hpp
@@ -29,7 +29,7 @@
 #ifndef __EventSystem_hpp__
 #define __EventSystem_hpp__
 
-#include <vector>
+#include <set>
 #include <boost/scoped_ptr.hpp>
 
 #include "Systems/Base/RLTimer.hpp"
@@ -212,8 +212,8 @@ public:
   EventSystemGlobals& globals() { return globals_; }
 
 protected:
-  typedef std::vector<EventHandler*> Handlers;
-  typedef std::vector<EventListener*> EventListeners;
+  typedef std::set<EventHandler*> Handlers;
+  typedef std::set<EventListener*> EventListeners;
 
   Handlers::iterator handlers_begin() { return event_handlers_.begin(); }
   Handlers::iterator handlers_end() { return event_handlers_.end(); }