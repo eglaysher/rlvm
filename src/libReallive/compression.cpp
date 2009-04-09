eglaysher/rlvm

commit 2524ca5028c57fcf364d693130deec61676425d5

Be able to read the CLANNAD_FV Seen.txt file.
Newer Reallive games appear to have per-game XOR keys, which have to be
reverse engineered one by one. Modify how our decompression stuff
works so that it accepts one of these keys.

Big thanks to the CLANNAD translation wiki for simply having this
information, especially the XOR key for CLANNAD; Haeleth's rldev
currently only has the key for Little Busters!

src/libReallive/compression.cpp
@@ -61,6 +61,18 @@ static char xor_mask[256] = {
   0xb0, 0x43, 0x00, 0x85, 0xff, 0x76, 0x49, 0x81, 0xff, 0x00, 0x00, 0x04, 0x00, 0x6a, 0x00, 0x76
 };
 
+/* In some new titles, a second round of XORing is performed on a block of
+ * uncompressed bytecode. The keys appear to be on a game-by-game basis. */
+const char little_busters_xor_mask_2[] = {
+  0xa8, 0x28, 0xfd, 0x66, 0xa0, 0x23, 0x77, 0x69, 0xf9, 0x45, 0xf8, 0x2c, 0x7c,
+  0x00, 0xad, 0xf4
+};
+
+const char clannad_full_voice_xor_mask_2[] = {
+  0xAF, 0x2F, 0xFB, 0x6B, 0xAF, 0x30, 0x77, 0x17, 0x87, 0x48, 0xFE, 0x2C, 0x68,
+  0x1A, 0xB9, 0xF0
+};
+
 // "Encrypt"/"decrypt" a file.
 void
 apply_mask(char* array, size_t len)
@@ -84,7 +96,8 @@ apply_mask(string& array, size_t start)
 
 // Decompress an archived file.
 void
-decompress(const char* src, size_t src_len, char* dst, size_t dst_len)
+decompress(const char* src, size_t src_len, char* dst, size_t dst_len,
+           const char* per_game_xor_key)
 {
   int bit = 1;
   const char* srcend = src + src_len;
@@ -112,6 +125,11 @@ decompress(const char* src, size_t src_len, char* dst, size_t dst_len)
         *dst++ = *repeat++;
     }
     bit <<= 1;
+  }
+
+  if (per_game_xor_key) {
+    dst = dststart + 256;
+    for (int i = 0; i < 257; ++i) *dst++ ^= per_game_xor_key[i % 16];
   }
 }
 

src/libReallive/archive.cpp
@@ -36,18 +36,29 @@
 // -----------------------------------------------------------------------
 
 #include "archive.h"
+#include "compression.h"
 #include "string.h"
 
 namespace libReallive {
 
-Archive::Archive(string filename) : name(filename), info(filename, Read)
+Archive::Archive(const string& filename)
+  : name(filename), info(filename, Read), second_level_xor_key_(NULL)
 {
-	// Read TOC
-	const char* idx = info.get();
-	for (int i = 0; i < 10000; ++i, idx += 8) {
-		const int offs = read_i32(idx);
-		if (offs) scenarios[i] = FilePos(info.get() + offs, read_i32(idx + 4));
-	}
+  readTOC();
+}
+
+Archive::Archive(const string& filename, const std::string& regname)
+  : name(filename), info(filename, Read), second_level_xor_key_(NULL)
+{
+  readTOC();
+
+  if (regname == "KEY\\CLANNAD_FV")
+    second_level_xor_key_ = 
+      libReallive::Compression::clannad_full_voice_xor_mask_2;
+  // WARNING: JUST A GUESS. I DON'T OWN A COPY OF LITTLE BUSTERS.
+  else if (regname == "KEY\\LITTLE_BUSTERS")
+    second_level_xor_key_ =
+      libReallive::Compression::little_busters_xor_mask_2;
 }
 
 Archive::~Archive()
@@ -55,12 +66,24 @@ Archive::~Archive()
 	for (accessed_t::iterator it = accessed.begin(); it != accessed.end(); ++it) delete it->second;
 }
 
+void Archive::readTOC() {
+	const char* idx = info.get();
+	for (int i = 0; i < 10000; ++i, idx += 8) {
+		const int offs = read_i32(idx);
+		if (offs)
+      scenarios[i] = FilePos(info.get() + offs, read_i32(idx + 4));
+	}
+}
+
 Scenario*
 Archive::scenario(int index) {
 	accessed_t::const_iterator at = accessed.find(index);
-	if (at != accessed.end()) return at->second;
+	if (at != accessed.end())
+    return at->second;
 	scenarios_t::const_iterator st = scenarios.find(index);
-	if (st != scenarios.end()) return accessed[index] = new Scenario(st->second, index);
+	if (st != scenarios.end())
+    return accessed[index] = 
+      new Scenario(st->second, index, second_level_xor_key_);
 	return NULL;
 }
 

src/libReallive/archive.h
@@ -44,15 +44,34 @@ namespace libReallive {
  * Interface to a loaded SEEN.TXT file.
  * 
  */
-  class Archive {
+class Archive {
   typedef std::map<int, FilePos> scenarios_t;
   typedef std::map<int, Scenario*> accessed_t;
   scenarios_t scenarios;
   accessed_t accessed;
   string name;
   Mapping info;
+
+  /**
+   * Now that VisualArts is using per game xor keys, this is equivalent to the
+   * game's second level xor key.
+   */
+  const char* second_level_xor_key_;
+
+  void readTOC();
+
 public:
-  Archive(string filename);
+  /// Read an archive, assuming no per-game xor key. (Used in unit testing).
+  Archive(const string& filename);
+
+  /** 
+   * Creates an interface to a SEEN.TXT file.
+   * 
+   * @param filename path to the SEEN.TXT file.
+   * @param regname The \#REGNAME key from the game's Gameexe.ini file. Required
+   *                to lookup per-game xor key for newer games.
+   */
+  Archive(const string& filename, const string& regname);
   ~Archive();
   
   typedef std::map<int, FilePos>::const_iterator const_iterator;
@@ -68,10 +87,6 @@ public:
   Scenario* scenario(int index);
   
   void reset();
-//  void commit();
-  
-  /// Rewrites an optimized form of 
-//  void write_to(string filename);
 };
 
 }

src/libReallive/compression.h
@@ -37,7 +37,12 @@ namespace Compression {
 
 typedef AVG32Comp::Compress<AVG32Comp::CInfoRealLive, AVG32Comp::Container::RLDataContainer> RealliveCompressor;
 
-void decompress(const char* src, size_t src_len, char* dst, size_t dst_len);
+// Per game xor keys to be passed to decompress.
+extern const char little_busters_xor_mask_2[];
+extern const char clannad_full_voice_xor_mask_2[];
+
+void decompress(const char* src, size_t src_len, char* dst, size_t dst_len,
+                const char* per_game_xor_key);
 string* compress(char* arr, size_t len);
 void apply_mask(char* array, size_t len);
 void apply_mask(string& array, size_t start = 0);

src/libReallive/scenario.cpp
@@ -59,8 +59,23 @@ Metadata::assign(const char* input)
   
 Header::Header(const char* data, const size_t length)
 {
-  if (length < 0x1d0 || read_i32(data + 4) != 10002)
+  if (length < 0x1d0)
     throw Error("not a RealLive bytecode file");
+
+  string compiler = string(data, 4);
+
+  // Check the version of the 
+  if(read_i32(data + 4) == 10002)
+    use_xor_2 = false;
+  else if(read_i32(data + 4) == 110002)
+    use_xor_2 = true;
+  else {
+    // New xor key?
+    ostringstream oss;
+    oss << "Unsupported compiler version: " << read_i32(data + 4);
+    throw Error(oss.str());
+  }
+
   if (read_i32(data) != 0x1d0)
     throw Error("unsupported bytecode version");
 
@@ -91,7 +106,8 @@ Header::Header(const char* data, const size_t length)
     rldev_metadata.assign(data + offs);
 }
 
-Script::Script(const Header& hdr, const char* data, const size_t length)
+Script::Script(const Header& hdr, const char* data, const size_t length,
+               const char* second_level_xor_key)
   : uptodate(true), strip(false)
 {
   // Kidoku/entrypoint table
@@ -109,7 +125,8 @@ Script::Script(const Header& hdr, const char* data, const size_t length)
   Compression::decompress(data + read_i32(data + 0x20),
                           read_i32(data + 0x28),
                           uncompressed,
-                          dlen);
+                          dlen,
+                          second_level_xor_key);
   // Read bytecode
   const char* stream = uncompressed;
   size_t pos = 0;

src/libReallive/scenario.h
@@ -46,8 +46,9 @@ class Scenario {
   Script script;
   int scenarioNum;
 public:
-  Scenario(const char* data, const size_t length, int scenarioNum);
-  Scenario(const FilePos& fp, int scenarioNum);
+  Scenario(const char* data, const size_t length, int scenarioNum, 
+           const char* second_level_xor_key);
+  Scenario(const FilePos& fp, int scenarioNum, const char* second_level_xor_key);
 
   // Get the scenario number
   int sceneNumber() const { return scenarioNum; }
@@ -92,14 +93,17 @@ public:
 // Inline definitions for Scenario
 
 inline
-Scenario::Scenario(const char* data, const size_t length, int sn) 
-  : header(data, length), script(header, data, length), scenarioNum(sn)
+Scenario::Scenario(const char* data, const size_t length, int sn,
+                   const char* second_level_xor_key)
+  : header(data, length), script(header, data, length, second_level_xor_key),
+    scenarioNum(sn)
 {
 }
 
 inline
-Scenario::Scenario(const FilePos& fp, int sn) 
-  : header(fp.data, fp.length), script(header, fp.data, fp.length),
+Scenario::Scenario(const FilePos& fp, int sn, const char* second_level_xor_key)
+  : header(fp.data, fp.length), script(header, fp.data, fp.length, 
+                                       second_level_xor_key),
     scenarioNum(sn)
 {
 }

src/libReallive/scenario_internals.h
@@ -49,6 +49,10 @@ class Header {
   friend class Scenario;
   Header(const char* data, const size_t length);
 public:
+  /// Starting around the release of Little Busters!, scenario files has a
+  /// second round of xor done to them. When will they learn?
+  bool use_xor_2;
+
   long zminusone, zminustwo, savepoint_message,
        savepoint_selcom, savepoint_seentop;
   std::vector<string> dramatis_personae;
@@ -71,7 +75,8 @@ private:
   BytecodeList elts;
   bool strip;
 
-  Script(const Header& hdr, const char* data, const size_t length);
+  Script(const Header& hdr, const char* data, const size_t length,
+         const char* second_level_xor_key);
 
   void recalculate(const bool force = false);
 

src/rlvm.cpp
@@ -347,7 +347,7 @@ int main(int argc, char* argv[])
     }
 
     SDLSystem sdlSystem(gameexe);
-    libReallive::Archive arc(seenPath.file_string());
+    libReallive::Archive arc(seenPath.file_string(), gameexe("REGNAME"));
     RLMachine rlmachine(sdlSystem, arc);
     addAllModules(rlmachine);
 

commit 937883cf8a98de438c644aba104d99d49fd28d45

Fix use of game specific XOR key so we don't corrupt the heap.
Crash introduced in 2524ca5028c57fcf364d693130deec61676425d5, where I
added per-game XOR key support for CLANNAD_FV.

src/libReallive/compression.cpp
@@ -129,7 +129,9 @@ decompress(const char* src, size_t src_len, char* dst, size_t dst_len,
 
   if (per_game_xor_key) {
     dst = dststart + 256;
-    for (int i = 0; i < 257; ++i) *dst++ ^= per_game_xor_key[i % 16];
+    for (int i = 0; i < 257 && dst < dstend; ++i) {
+      *dst++ ^= per_game_xor_key[i % 16];
+    }
   }
 }
 