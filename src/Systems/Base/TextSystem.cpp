eglaysher/rlvm

commit 6ffac493b2add08ad8cd7d11269df6d10e0e1f40

Redo the textwindow manipulation code in EventLoop.
This fixes the message box not being shown once a game is saved in CLANNAD_FV
and I think it's closer to the RealLive model.

(I think 28b2a7659752819c3137c23ccabf3f92793299eb was wrong; this replaces that
work and also simplifies some of the code.)

src/Systems/Base/TextSystem.cpp
@@ -133,19 +133,23 @@ TextSystem::~TextSystem() {
 
 void TextSystem::executeTextSystem() {
   // Check to see if the cursor is displayed
-  WindowMap::iterator it = text_window_.find(active_window_);
-  if (it != text_window_.end() && it->second->isVisible() &&
-      in_pause_state_ && !isReadingBacklog()) {
-    if (!text_key_cursor_)
-      setKeyCursor(0);
+  if (showWindow(active_window_)) {
+    WindowMap::iterator it = text_window_.find(active_window_);
+    if (it != text_window_.end() && it->second->isVisible() &&
+        in_pause_state_ && !isReadingBacklog()) {
+      if (!text_key_cursor_)
+        setKeyCursor(0);
 
-    text_key_cursor_->execute();
+      text_key_cursor_->execute();
+    }
   }
 
   // Let each window update any TextWindowButton s.
   for (WindowMap::iterator it = text_window_.begin(); it != text_window_.end();
        ++it) {
-    it->second->execute();
+    if (showWindow(it->first)) {
+      it->second->execute();
+    }
   }
 }
 
@@ -159,28 +163,38 @@ void TextSystem::render(std::ostream* tree) {
 
     for (WindowMap::iterator it = text_window_.begin();
          it != text_window_.end(); ++it) {
-      it->second->render(tree);
+      if (showWindow(it->first)) {
+        it->second->render(tree);
+      }
     }
 
-    WindowMap::iterator it = text_window_.find(active_window_);
+    if (showWindow(active_window_)) {
+      WindowMap::iterator it = text_window_.find(active_window_);
 
-    if (it != text_window_.end() && it->second->isVisible() &&
-        in_pause_state_ && !isReadingBacklog()) {
-      if (!text_key_cursor_)
-        setKeyCursor(0);
+      if (it != text_window_.end() && it->second->isVisible() &&
+          in_pause_state_ && !isReadingBacklog()) {
+        if (!text_key_cursor_)
+          setKeyCursor(0);
 
-      text_key_cursor_->render(*it->second, tree);
+        text_key_cursor_->render(*it->second, tree);
+      }
     }
   }
 }
 
 // -----------------------------------------------------------------------
 
-void TextSystem::hideTextWindow(int win_number) {
+void TextSystem::closeTextWindow(int win_number) {
   WindowMap::iterator it = text_window_.find(win_number);
   if (it != text_window_.end()) {
-    it->second->setVisible(0);
+    text_window_.erase(it);
   }
+}
+
+// -----------------------------------------------------------------------
+
+void TextSystem::closeAllTextWindows() {
+  text_window_.clear();
 }
 
 // -----------------------------------------------------------------------
@@ -194,29 +208,22 @@ void TextSystem::hideAllTextWindows() {
 
 // -----------------------------------------------------------------------
 
-void TextSystem::showTextWindow(int win_number) {
-  WindowMap::iterator it = text_window_.find(win_number);
-  if (it != text_window_.end()) {
-    it->second->setVisible(1);
+void TextSystem::setVisualOverride(int win_number, bool show_window) {
+  window_visual_override_[win_number] = show_window;
+}
+
+// -----------------------------------------------------------------------
+
+void TextSystem::setVisualOverrideAll(bool show_window) {
+  for (int i = 0; i < 64; ++i) {
+    window_visual_override_[i] = show_window;
   }
 }
 
 // -----------------------------------------------------------------------
 
-void TextSystem::showAllTextWindows() {
-  for (WindowMap::iterator it = text_window_.begin(); it != text_window_.end();
-       ++it) {
-    it->second->setVisible(1);
-  }
-}
-
-// -----------------------------------------------------------------------
-
-void TextSystem::clearAllTextWindows() {
-  for (WindowMap::iterator it = text_window_.begin(); it != text_window_.end();
-       ++it) {
-    it->second->clearWin();
-  }
+void TextSystem::clearVisualOverrides() {
+  window_visual_override_.clear();
 }
 
 // -----------------------------------------------------------------------
@@ -317,8 +324,7 @@ void TextSystem::backPage() {
     previous_page_it_ = boost::prior(previous_page_it_);
 
     // Clear all windows
-    clearAllTextWindows();
-    hideAllTextWindows();
+    closeAllTextWindows();
 
     replayPageSet(*previous_page_it_, false);
   }
@@ -333,8 +339,7 @@ void TextSystem::forwardPage() {
     previous_page_it_ = boost::next(previous_page_it_);
 
     // Clear all windows
-    clearAllTextWindows();
-    hideAllTextWindows();
+    closeAllTextWindows();
 
     if (previous_page_it_ != previous_page_sets_.end())
       replayPageSet(*previous_page_it_, false);
@@ -372,8 +377,7 @@ void TextSystem::stopReadingBacklog() {
   is_reading_backlog_ = false;
 
   // Clear all windows
-  clearAllTextWindows();
-  hideAllTextWindows();
+  closeAllTextWindows();
   replayPageSet(*current_pageset_, true);
 }
 
@@ -424,6 +428,17 @@ void TextSystem::updateWindowsForChangeToWindowAttr() {
 
 // -----------------------------------------------------------------------
 
+bool TextSystem::showWindow(int win_num) const {
+  std::map<int, bool>::const_iterator it =
+      window_visual_override_.find(win_num);
+  if (it != window_visual_override_.end())
+    return it->second;
+  else
+    return true;
+}
+
+// -----------------------------------------------------------------------
+
 void TextSystem::setDefaultWindowAttr(const std::vector<int>& attr) {
   globals_.window_attr = attr;
   updateWindowsForChangeToWindowAttr();
@@ -469,7 +484,9 @@ void TextSystem::setWindowAttrF(int i) {
 void TextSystem::setMousePosition(const Point& pos) {
   for (WindowMap::iterator it = text_window_.begin();
        it != text_window_.end(); ++it) {
-    it->second->setMousePosition(pos);
+    if (showWindow(it->first)) {
+      it->second->setMousePosition(pos);
+    }
   }
 }
 
@@ -480,8 +497,10 @@ bool TextSystem::handleMouseClick(RLMachine& machine, const Point& pos,
   if (systemVisible()) {
     for (WindowMap::iterator it = text_window_.begin();
          it != text_window_.end(); ++it) {
-      if (it->second->handleMouseClick(machine, pos, pressed))
-        return true;
+      if (showWindow(it->first)) {
+        if (it->second->handleMouseClick(machine, pos, pressed))
+          return true;
+      }
     }
   }
 

src/Modules/Module_EventLoop.cpp
@@ -32,7 +32,31 @@
 #include "Modules/Module_EventLoop.hpp"
 #include "MachineBase/RLMachine.hpp"
 #include "MachineBase/GeneralOperations.hpp"
+#include "Systems/Base/System.hpp"
 #include "Systems/Base/TextSystem.hpp"
+
+// -----------------------------------------------------------------------
+
+struct EventLoop_setOverride : public RLOp_Void_1< IntConstant_T > {
+  bool value_;
+  EventLoop_setOverride(int value) : value_(value) { }
+
+  void operator()(RLMachine& machine, int window) {
+    machine.system().text().setVisualOverride(window, value_);
+  }
+};
+
+// -----------------------------------------------------------------------
+
+struct EventLoop_setOverrideAll : public RLOp_Void_Void {
+  bool value_;
+  EventLoop_setOverrideAll(int value) : value_(value) { }
+
+  void operator()(RLMachine& machine) {
+    machine.system().text().setVisualOverrideAll(value_);
+  }
+};
+
 
 // -----------------------------------------------------------------------
 
@@ -57,12 +81,14 @@ EventLoopModule::EventLoopModule()
 
   // opcode<0:4:1202, 0> and opcode<0:4:1200, 0> are used in the CLANNAD menu
   // system; no idea what they do.
-  addOpcode(1200, 0, "TextwindowShow",
-            callFunction(&TextSystem::showTextWindow));
-  addOpcode(1200, 2, "TextwindowShow",
-            callFunction(&TextSystem::showAllTextWindows));
-  addOpcode(1201, 0, "TextwindowHide",
-            callFunction(&TextSystem::hideTextWindow));
-  addOpcode(1201, 2, "TextwindowHide",
-            callFunction(&TextSystem::hideAllTextWindows));
+  addOpcode(1200, 0, "TextwindowOverrideShow",
+            new EventLoop_setOverride(true));
+  addOpcode(1200, 2, "TextwindowOverrideShow",
+            new EventLoop_setOverrideAll(true));
+  addOpcode(1201, 0, "TextwindowOverrideHide",
+            new EventLoop_setOverride(false));
+  addOpcode(1201, 2, "TextwindowOverrideHide",
+            new EventLoop_setOverrideAll(false));
+  addOpcode(1202, 0, "ClearTextwindowOverrides",
+            callFunction(&TextSystem::clearVisualOverrides));
 }

src/Modules/Module_Msg.cpp
@@ -150,8 +150,7 @@ struct Msg_msgHide : public RLOp_Void_1< DefaultIntValue_T< 0 > >
   void operator()(RLMachine& machine, int unknown)
   {
     int winNum = machine.system().text().activeWindow();
-    machine.system().text().hideTextWindow(winNum);
-    machine.system().text().newPageOnWindow(winNum);
+    machine.system().text().closeTextWindow(winNum);
   }
 };
 
@@ -164,8 +163,7 @@ struct Msg_msgHideAll : public RLOp_Void_Void {
     vector<int> activeWindows = text.activeWindows();
     for(vector<int>::const_iterator it = activeWindows.begin();
         it != activeWindows.end(); ++it) {
-      text.hideTextWindow(*it);
-      text.newPageOnWindow(*it);
+      machine.system().text().closeTextWindow(*it);
     }
   }
 };

src/Systems/Base/TextSystem.hpp
@@ -39,6 +39,7 @@
 #include <boost/signal.hpp>
 #include <string>
 #include <vector>
+#include <map>
 
 class Gameexe;
 class Memory;
@@ -107,11 +108,13 @@ public:
   void executeTextSystem();
 
   void render(std::ostream* tree);
-  void hideTextWindow(int win_number);
+  void closeTextWindow(int win_number);
+  void closeAllTextWindows();
   void hideAllTextWindows();
-  void showTextWindow(int win_number);
-  void showAllTextWindows();
-  void clearAllTextWindows();
+  void setVisualOverride(int win_number, bool show_window);
+  void setVisualOverrideAll(bool show_window);
+  void clearVisualOverrides();
+
   virtual boost::shared_ptr<TextWindow> textWindow(int text_window_number) = 0;
   boost::shared_ptr<TextWindow> currentWindow();
 
@@ -288,6 +291,8 @@ public:
 protected:
   void updateWindowsForChangeToWindowAttr();
 
+  bool showWindow(int win_num) const;
+
   /// TextPage will call our internals since it actually does most of
   /// the work while we hold state.
   friend class TextPage;
@@ -386,6 +391,9 @@ protected:
 
   /// Whether we are currently paused at a user choice.
   bool in_selection_mode_;
+
+  /// Contains overrides for showing or hiding the text windows.
+  std::map<int, bool> window_visual_override_;
 
   /**
    * Reduces the number of page snapshots in previous_page_sets_ down to a

commit fc22fa38483ad89da54783381d43b2800bae3751

Fix loading save games from inside CLANNAD's menu.
(My new override code wasn't clearing itself during reset(), keeping the
window hidden even after a new savegame was loaded.)

Bug introduced in 6ffac493b2add08ad8cd7d11269df6d10e0e1f40.

src/Systems/Base/TextSystem.cpp
@@ -516,6 +516,7 @@ void TextSystem::reset() {
   previous_page_sets_.clear();
   previous_page_it_ = previous_page_sets_.end();
 
+  window_visual_override_.clear();
   text_window_.clear();
 
   system_visible_ = true;