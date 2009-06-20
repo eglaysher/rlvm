eglaysher/rlvm

commit f59b3d6d657496d483647abaadcba3e99cf87eb8

Completely remove RLMachine from the GraphicsObjectData hiearchy.

src/Systems/Base/GraphicsObjectData.cpp
@@ -187,7 +187,7 @@ int GraphicsObjectData::getRenderingAlpha(const GraphicsObject& go)
 
 // -----------------------------------------------------------------------
 
-void GraphicsObjectData::execute(RLMachine& machine) { }
+void GraphicsObjectData::execute() { }
 
 // -----------------------------------------------------------------------
 
@@ -198,7 +198,7 @@ bool GraphicsObjectData::isAnimation() const
 
 // -----------------------------------------------------------------------
 
-void GraphicsObjectData::playSet(RLMachine& machine, int set) { }
+void GraphicsObjectData::playSet(int set) { }
 
 // -----------------------------------------------------------------------
 

src/Systems/Base/GraphicsObjectData.hpp
@@ -35,7 +35,6 @@
 
 class GraphicsObject;
 class Point;
-class RLMachine;
 class Rect;
 class Surface;
 
@@ -76,10 +75,10 @@ public:
 
   virtual GraphicsObjectData* clone() const = 0;
 
-  virtual void execute(RLMachine& machine);
+  virtual void execute();
 
   virtual bool isAnimation() const;
-  virtual void playSet(RLMachine& machine, int set);
+  virtual void playSet(int set);
 
   /// Returns when an animation has completed. (This only returns true when
   /// afterAnimation() is set to AFTER_NONE.)

src/Systems/Base/GraphicsObjectOfFile.cpp
@@ -54,11 +54,12 @@ namespace fs = boost::filesystem;
 
 // -----------------------------------------------------------------------
 
-GraphicsObjectOfFile::GraphicsObjectOfFile()
-  : filename_(""),
-    frame_time_(0),
-    current_frame_(0),
-    time_at_last_frame_change_(0)
+GraphicsObjectOfFile::GraphicsObjectOfFile(System& system)
+    : system_(system),
+      filename_(""),
+      frame_time_(0),
+      current_frame_(0),
+      time_at_last_frame_change_(0)
 {
 }
 
@@ -67,6 +68,7 @@ GraphicsObjectOfFile::GraphicsObjectOfFile()
 GraphicsObjectOfFile::GraphicsObjectOfFile(
   const GraphicsObjectOfFile& obj)
   : GraphicsObjectData(obj),
+    system_(obj.system_),
     filename_(obj.filename_),
     surface_(obj.surface_),
     frame_time_(obj.frame_time_),
@@ -77,20 +79,20 @@ GraphicsObjectOfFile::GraphicsObjectOfFile(
 // -----------------------------------------------------------------------
 
 GraphicsObjectOfFile::GraphicsObjectOfFile(
-  RLMachine& machine, const std::string& filename)
-  : filename_(filename),
-    frame_time_(0),
-    current_frame_(0),
-    time_at_last_frame_change_(0)
-{
-  loadFile(machine);
+    System& system, const std::string& filename)
+    : system_(system),
+      filename_(filename),
+      frame_time_(0),
+      current_frame_(0),
+      time_at_last_frame_change_(0) {
+  loadFile();
 }
 
 // -----------------------------------------------------------------------
 
-void GraphicsObjectOfFile::loadFile(RLMachine& machine)
+void GraphicsObjectOfFile::loadFile()
 {
-  surface_ = machine.system().graphics().loadSurfaceFromFile(machine, filename_);
+  surface_ = system_.graphics().loadNonCGSurfaceFromFile(filename_);
 }
 
 // -----------------------------------------------------------------------
@@ -124,7 +126,7 @@ void GraphicsObjectOfFile::execute(RLMachine& machine)
 {
   if(currentlyPlaying())
   {
-    unsigned int current_time = machine.system().event().getTicks();
+    unsigned int current_time = system_.event().getTicks();
     unsigned int time_since_last_frame_change =
       current_time - time_at_last_frame_change_;
 
@@ -139,7 +141,7 @@ void GraphicsObjectOfFile::execute(RLMachine& machine)
 
       time_at_last_frame_change_ += frame_time_;
       time_since_last_frame_change = current_time - time_at_last_frame_change_;
-      machine.system().graphics().markScreenAsDirty(GUT_DISPLAY_OBJ);
+      system_.graphics().markScreenAsDirty(GUT_DISPLAY_OBJ);
     }
   }
 }
@@ -198,8 +200,8 @@ void GraphicsObjectOfFile::playSet(RLMachine& machine, int frame_time)
     frame_time_ = 10;
   }
 
-  time_at_last_frame_change_ = machine.system().event().getTicks();
-  machine.system().graphics().markScreenAsDirty(GUT_DISPLAY_OBJ);
+  time_at_last_frame_change_ = system_.event().getTicks();
+  system_.graphics().markScreenAsDirty(GUT_DISPLAY_OBJ);
 }
 
 // -----------------------------------------------------------------------
@@ -210,7 +212,7 @@ void GraphicsObjectOfFile::load(Archive& ar, unsigned int version)
   ar & boost::serialization::base_object<GraphicsObjectData>(*this)
     & filename_ & frame_time_ & current_frame_ & time_at_last_frame_change_;
 
-  loadFile(*Serialization::g_current_machine);
+  loadFile();
 }
 
 // -----------------------------------------------------------------------

src/Systems/Base/GraphicsObjectOfFile.hpp
@@ -31,6 +31,8 @@
 
 #include "Systems/Base/GraphicsObjectData.hpp"
 
+#include "MachineBase/Serialization.hpp"
+
 #include <boost/serialization/access.hpp>
 #include <boost/serialization/split_member.hpp>
 #include <boost/shared_ptr.hpp>
@@ -51,10 +53,10 @@ class RLMachine;
 class GraphicsObjectOfFile : public GraphicsObjectData
 {
 public:
-  GraphicsObjectOfFile();
-  GraphicsObjectOfFile(RLMachine& machine, const std::string& filename);
+  GraphicsObjectOfFile(System& system);
+  GraphicsObjectOfFile(System& system, const std::string& filename);
 
-  void loadFile(RLMachine& machine);
+  void loadFile();
 
   const std::string& filename() const { return filename_; }
 
@@ -77,6 +79,9 @@ protected:
 private:
   // Private constructor for cloning
   GraphicsObjectOfFile(const GraphicsObjectOfFile& obj);
+
+  /// Our parent system.
+  System& system_;
 
   /// The name of the graphics file that was loaded.
   std::string filename_;
@@ -106,5 +111,19 @@ private:
 
   BOOST_SERIALIZATION_SPLIT_MEMBER()
 };
+
+
+/**
+ * We need help creating AnmGraphicsObjectData s since they don't have a
+ * default constructor:
+ */
+namespace boost { namespace serialization {
+template<class Archive>
+inline void load_construct_data(
+  Archive & ar, GraphicsObjectOfFile* t, const unsigned int file_version)
+{
+  ::new(t)GraphicsObjectOfFile(Serialization::g_current_machine->system());
+}
+  }}
 
 #endif

src/Modules/Module_Gan.cpp
@@ -116,7 +116,7 @@ struct Gan_ganPlay : public RLOp_Void_2<IntConstant_T, IntConstant_T>
       GraphicsObjectData& data = obj.objectData();
       if(data.isAnimation())
       {
-        data.playSet(machine, animationSet);
+        data.playSet(animationSet);
         data.setAfterAction(after_effect_);
 
         if(block_)

src/Modules/Module_ObjCreation.cpp
@@ -70,7 +70,7 @@ void setObjectDataToGan(
   if(imgFilename == "???")
     imgFilename = ganFilename;
   obj.setObjectData(
-    new GanGraphicsObjectData(machine, ganFilename, imgFilename));
+      new GanGraphicsObjectData(machine.system(), ganFilename, imgFilename));
 }
 
 // -----------------------------------------------------------------------
@@ -81,8 +81,7 @@ typedef boost::function<void(RLMachine&, GraphicsObject& obj, const string&)> Da
 
 void objOfFileLoader(RLMachine& machine, GraphicsObject& obj, const string& val)
 {
-  GraphicsSystem& gs = machine.system().graphics();
-  obj.setObjectData(gs.buildObjOfFile(machine, val));
+  obj.setObjectData(machine.system().graphics().buildObjOfFile(val));
 }
 
 // -----------------------------------------------------------------------

src/Systems/Base/AnmGraphicsObjectData.cpp
@@ -49,7 +49,6 @@
 #include <boost/serialization/export.hpp>
 #include <boost/serialization/scoped_ptr.hpp>
 
-#include "MachineBase/RLMachine.hpp"
 #include "MachineBase/Serialization.hpp"
 #include "Systems/Base/AnmGraphicsObjectData.hpp"
 #include "Systems/Base/EventSystem.hpp"
@@ -87,17 +86,16 @@ static const char ANM_MAGIC[ANM_MAGIC_SIZE] =
 
 // -----------------------------------------------------------------------
 
-AnmGraphicsObjectData::AnmGraphicsObjectData()
-  : current_set_(-1)
+AnmGraphicsObjectData::AnmGraphicsObjectData(System& system)
+    : system_(system), current_set_(-1)
 {}
 
 // -----------------------------------------------------------------------
 
 AnmGraphicsObjectData::AnmGraphicsObjectData(
-  RLMachine& machine, const std::string& file)
-  : filename_(file), current_set_(-1)
-{
-  loadAnmFile(machine);
+    System& system, const std::string& file)
+    : system_(system), filename_(file), current_set_(-1) {
+  loadAnmFile();
 }
 
 // -----------------------------------------------------------------------
@@ -114,9 +112,9 @@ bool AnmGraphicsObjectData::testFileMagic(boost::scoped_array<char>& anm_data)
 
 // -----------------------------------------------------------------------
 
-void AnmGraphicsObjectData::loadAnmFile(RLMachine& machine)
+void AnmGraphicsObjectData::loadAnmFile()
 {
-  fs::path file = findFile(machine, filename_, ANM_FILETYPES);
+  fs::path file = findFile(system_, filename_, ANM_FILETYPES);
 
   fs::ifstream ifs(file, ifstream::in | ifstream::binary);
   if(!ifs)
@@ -142,13 +140,13 @@ void AnmGraphicsObjectData::loadAnmFile(RLMachine& machine)
     throw rlvm::Exception(oss.str());
   }
 
-  loadAnmFileFromData(machine, anm_data);
+  loadAnmFileFromData(anm_data);
 }
 
 // -----------------------------------------------------------------------
 
 void AnmGraphicsObjectData::loadAnmFileFromData(
-  RLMachine& machine, boost::scoped_array<char>& anm_data)
+    boost::scoped_array<char>& anm_data)
 {
   const char* data = anm_data.get();
 
@@ -161,12 +159,11 @@ void AnmGraphicsObjectData::loadAnmFileFromData(
 
   // Read the corresponding image file we read from, and load the image.
   string raw_file_name = data + 0x1c;
-  image = machine.system().graphics().loadSurfaceFromFile(
-    machine, raw_file_name);
+  image = system_.graphics().loadNonCGSurfaceFromFile(raw_file_name);
 
   // Read the frame list
   const char* buf = data + 0xb8;
-  Size screen_size = getScreenSize(machine.system().gameexe());
+  Size screen_size = getScreenSize(system_.gameexe());
   for(int i = 0; i < frames_len; ++i)
   {
     Frame f;
@@ -233,19 +230,19 @@ void AnmGraphicsObjectData::fixAxis(Frame& frame, int width, int height)
 
 // -----------------------------------------------------------------------
 
-void AnmGraphicsObjectData::execute(RLMachine& machine)
+void AnmGraphicsObjectData::execute()
 {
   if(currentlyPlaying())
-    advanceFrame(machine);
+    advanceFrame();
 }
 
 // -----------------------------------------------------------------------
 
-void AnmGraphicsObjectData::advanceFrame(RLMachine& machine)
+void AnmGraphicsObjectData::advanceFrame()
 {
   // Do things that advance the state
   int time_since_last_frame_change =
-    machine.system().event().getTicks() - time_at_last_frame_change_;
+    system_.event().getTicks() - time_at_last_frame_change_;
   bool done = false;
 
   while(currentlyPlaying() && !done)
@@ -254,7 +251,7 @@ void AnmGraphicsObjectData::advanceFrame(RLMachine& machine)
     {
       time_since_last_frame_change -= frames[current_frame_].time;
       time_at_last_frame_change_ += frames[current_frame_].time;
-      machine.system().graphics().markScreenAsDirty(GUT_DISPLAY_OBJ);
+      system_.graphics().markScreenAsDirty(GUT_DISPLAY_OBJ);
 
       cur_frame_++;
       if(cur_frame_ == cur_frame_end_)
@@ -306,10 +303,10 @@ GraphicsObjectData* AnmGraphicsObjectData::clone() const
 
 // -----------------------------------------------------------------------
 
-void AnmGraphicsObjectData::playSet(RLMachine& machine, int set)
+void AnmGraphicsObjectData::playSet(int set)
 {
   setCurrentlyPlaying(true);
-  time_at_last_frame_change_ = machine.system().event().getTicks();
+  time_at_last_frame_change_ = system_.event().getTicks();
 
   cur_frame_set_ = animation_set.at(set).begin();
   cur_frame_set_end_ = animation_set.at(set).end();
@@ -317,7 +314,7 @@ void AnmGraphicsObjectData::playSet(RLMachine& machine, int set)
   cur_frame_end_ = framelist.at(*cur_frame_set_).end();
   current_frame_ = *cur_frame_;
 
-  machine.system().graphics().markScreenAsDirty(GUT_DISPLAY_OBJ);
+  system_.graphics().markScreenAsDirty(GUT_DISPLAY_OBJ);
 }
 
 // -----------------------------------------------------------------------
@@ -371,7 +368,7 @@ void AnmGraphicsObjectData::load(Archive& ar, unsigned int version)
   ar & filename_;
 
   // Reconstruct the ANM data from whatever file was linked.
-  loadAnmFile(*Serialization::g_current_machine);
+  loadAnmFile();
 
   // Now load the rest of the data.
   ar & currently_playing_ & current_set_;

src/Systems/Base/AnmGraphicsObjectData.hpp
@@ -40,7 +40,6 @@
 // -----------------------------------------------------------------------
 
 class Surface;
-class RLMachine;
 
 // -----------------------------------------------------------------------
 
@@ -52,20 +51,20 @@ class RLMachine;
  */
 class AnmGraphicsObjectData : public GraphicsObjectData {
 public:
-  AnmGraphicsObjectData();
-  AnmGraphicsObjectData(RLMachine& machine, const std::string& file);
+  AnmGraphicsObjectData(System& system);
+  AnmGraphicsObjectData(System& system, const std::string& file);
   ~AnmGraphicsObjectData();
 
-  void loadAnmFile(RLMachine& machine);
+  void loadAnmFile();
 
   virtual int pixelWidth(const GraphicsObject& rendering_properties);
   virtual int pixelHeight(const GraphicsObject& rendering_properties);
 
   virtual GraphicsObjectData* clone() const;
-  virtual void execute(RLMachine& machine);
+  virtual void execute();
 
   virtual bool isAnimation() const { return true; }
-  virtual void playSet(RLMachine& machine, int set);
+  virtual void playSet(int set);
 
 protected:
   virtual boost::shared_ptr<Surface> currentSurface(const GraphicsObject& go);
@@ -76,7 +75,7 @@ protected:
 
 private:
   /// Advance the position in the animation.
-  void advanceFrame(RLMachine& machine);
+  void advanceFrame();
 
   /**
    * @name Data loading functions
@@ -95,9 +94,12 @@ private:
     const char* start, int offset, int iterations,
     std::vector< std::vector<int> >& dest);
   void loadAnmFileFromData(
-    RLMachine& machine, boost::scoped_array<char>& anm_data);
+      boost::scoped_array<char>& anm_data);
   void fixAxis(Frame& frame, int width, int height);
   /// @}
+
+  /// The system we are a part of.
+  System& system_;
 
   /// Raw, short name for the ANM file.
   std::string filename_;
@@ -149,5 +151,18 @@ private:
   BOOST_SERIALIZATION_SPLIT_MEMBER()
 };
 
+
+/**
+ * We need help creating AnmGraphicsObjectData s since they don't have a
+ * default constructor:
+ */
+namespace boost { namespace serialization {
+template<class Archive>
+inline void load_construct_data(
+  Archive & ar, AnmGraphicsObjectData* t, const unsigned int file_version)
+{
+  ::new(t)AnmGraphicsObjectData(Serialization::g_current_machine->system());
+}
+  }}
 
 #endif

commit d6b2df9e52b97d7c0aa6cceee93fb55f6ed720da

Fix regression where animated G00 files weren't being played.
This fixes a regression where many background animations in Planetarian
weren't being played.

(Bug introduced in f59b3d6d657496d483647abaadcba3e99cf87eb8. It appears
that I didn't fully rip out RLMachine references...)

src/Systems/Base/GraphicsObjectData.cpp
@@ -187,10 +187,6 @@ int GraphicsObjectData::getRenderingAlpha(const GraphicsObject& go)
 
 // -----------------------------------------------------------------------
 
-void GraphicsObjectData::execute() { }
-
-// -----------------------------------------------------------------------
-
 bool GraphicsObjectData::isAnimation() const
 {
   return false;

src/Systems/Base/GraphicsObjectData.hpp
@@ -75,7 +75,7 @@ public:
 
   virtual GraphicsObjectData* clone() const = 0;
 
-  virtual void execute();
+  virtual void execute() = 0;
 
   virtual bool isAnimation() const;
   virtual void playSet(int set);

src/Systems/Base/GraphicsObjectOfFile.cpp
@@ -122,7 +122,7 @@ GraphicsObjectData* GraphicsObjectOfFile::clone() const
 
 // -----------------------------------------------------------------------
 
-void GraphicsObjectOfFile::execute(RLMachine& machine)
+void GraphicsObjectOfFile::execute()
 {
   if(currentlyPlaying())
   {
@@ -187,7 +187,7 @@ void GraphicsObjectOfFile::objectInfo(std::ostream& tree)
 
 // -----------------------------------------------------------------------
 
-void GraphicsObjectOfFile::playSet(RLMachine& machine, int frame_time)
+void GraphicsObjectOfFile::playSet(int frame_time)
 {
   setCurrentlyPlaying(true);
   frame_time_ = frame_time;

src/Systems/Base/GraphicsObjectOfFile.hpp
@@ -67,10 +67,10 @@ public:
 
   virtual GraphicsObjectData* clone() const;
 
-  virtual void execute(RLMachine& machine);
+  virtual void execute();
 
   virtual bool isAnimation() const;
-  virtual void playSet(RLMachine& machine, int set);
+  virtual void playSet(int set);
 
 protected:
   virtual void loopAnimation();

src/Systems/Base/GraphicsTextObject.hpp
@@ -55,6 +55,7 @@ public:
   virtual int pixelHeight(const GraphicsObject& rendering_properties);
 
   virtual GraphicsObjectData* clone() const;
+  virtual void execute() { }
 
 protected:
   virtual boost::shared_ptr<Surface> currentSurface(const GraphicsObject& go);