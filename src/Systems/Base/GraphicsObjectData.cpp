eglaysher/rlvm

commit 65faeb2a9b4eb4970429592ec72b113a9328db46

Move the clipping calulations out of Texture.
Now part of the render tree output.

src/Systems/Base/GraphicsObjectData.cpp
@@ -67,38 +67,45 @@ void GraphicsObjectData::render(RLMachine& machine, const GraphicsObject& go,
     Rect dst = dstRect(go);
     int alpha = getRenderingAlpha(go);
 
-    /*
-     * NEXT TODO: Move this code block here from
-     * Texture::renderToScreenAsObject.
-     * 
-  // If clipping is active for this object, take that into account too.
-  if (go.hasClip()) {
-    // Do nothing if object falls wholly outside clip area
-    if (xPos2 < go.clipX1() || xPos1 > go.clipX2() ||
-        yPos2 < go.clipY1() || yPos1 > go.clipY2()) {
-      return;
+    if (tree) {
+      *tree << "  Rendering " << src << " to " << dst;
+      if (alpha != 255)
+        *tree << " (alpha=" << alpha << ")";
+      *tree << endl;
     }
-    // Otherwise, adjust coordinates to present only the visible area.
-    // POINT
-    // TODO: Move this logic into an intersection of rectangles.
-    if (xPos1 < go.clipX1()) {
-      xSrc1 += go.clipX1() - xPos1;
-      xPos1 = go.clipX1();
+
+    // Perform the object clipping.
+    if (go.hasClip()) {
+      // Do nothing if object falls wholly outside clip area
+      if (dst.x2() < go.clipX1() || dst.x() > go.clipX2() ||
+          dst.y2() < go.clipY1() || dst.y() > go.clipY2())
+        return;
+
+      // Otherwise, adjust coordinates to present only the visible area.
+      if (dst.x() < go.clipX1()) {
+        src.setX( src.x() + go.clipX1() - dst.x());
+        dst.setX(go.clipX1());
+      }
+      if (dst.y() < go.clipY1()) {
+        src.setY(src.y() + go.clipY1() - dst.y());
+        dst.setY(go.clipY1());
+      }
+      if (dst.x2() >= go.clipX2()) {
+        src.setX2(src.x2() - dst.x2() + go.clipX2());
+        dst.setX2(go.clipX2());
+      }
+      if (dst.y2() >= go.clipY2()) {
+        src.setY2(src.y2() - dst.y2() + go.clipY2());
+        dst.setY2(go.clipY2());
+      }
+
+      if (tree) {
+        *tree << "  After clipping rect, " << src << " to " << dst;
+        if (alpha != 255)
+          *tree << " (alpha=" << alpha << ")";
+        *tree << endl;
+      }
     }
-    if (yPos1 < go.clipY1()) {
-      ySrc1 += go.clipY1() - yPos1;
-      yPos1 = go.clipY1();
-    }
-    if (xPos2 >= go.clipX2()) {
-      xSrc2 -= xPos2 - go.clipX2();
-      xPos2 = go.clipX2();
-    }
-    if (yPos2 >= go.clipY2()) {
-      ySrc2 -= yPos2 - go.clipY2();
-      yPos2 = go.clipY2();
-    }
-  }
-     */
 
     surface->renderToScreenAsObject(go, src, dst, alpha);
   }

src/Systems/Base/Rect.hpp
@@ -40,7 +40,9 @@ public:
   Point(int x, int y) : x_(x), y_(y) {}
 
   int x() const { return x_; }
+  void setX(const int in) { x_ = in; }
   int y() const { return y_; }
+  void setY(const int in) { y_ = in; }
 
   bool isEmpty() const { return x_ == 0 && y_ == 0; }
 
@@ -172,15 +174,19 @@ public:
   }
 
   int x() const { return origin_.x(); }
+  void setX(const int in) { origin_.setX(in); }
   int y() const { return origin_.y(); }
-  const Point& origin() const { return origin_; }
-
+  void setY(const int in) { origin_.setY(in); }
   int x2() const { return origin_.x() + size_.width(); }
+  void setX2(const int in) { size_.setWidth(in - origin_.x()); }
   int y2() const { return origin_.y() + size_.height(); }
+  void setY2(const int in) { size_.setHeight(in - origin_.y()); }
 
   int width() const { return size_.width(); }
   int height() const { return size_.height(); }
+
   const Size& size() const { return size_; }
+  const Point& origin() const { return origin_; }
 
   bool isEmpty() const { return origin_.isEmpty() && size_.isEmpty(); }
 

src/Systems/SDL/Texture.cpp
@@ -663,40 +663,9 @@ void Texture::renderToScreenAsObject(
   int ySrc1 = srcRect.y();
   int xSrc2 = srcRect.x2();
   int ySrc2 = srcRect.y2();
-  int xPos1 = dstRect.x();
-  int yPos1 = dstRect.y();
-  int xPos2 = dstRect.x2();
-  int yPos2 = dstRect.y2();
 
-  // If clipping is active for this object, take that into account too.
-  if (go.hasClip()) {
-    // Do nothing if object falls wholly outside clip area
-    if (xPos2 < go.clipX1() || xPos1 > go.clipX2() ||
-        yPos2 < go.clipY1() || yPos1 > go.clipY2()) {
-      return;
-    }
-    // Otherwise, adjust coordinates to present only the visible area.
-    // POINT
-    // TODO: Move this logic into an intersection of rectangles.
-    if (xPos1 < go.clipX1()) {
-      xSrc1 += go.clipX1() - xPos1;
-      xPos1 = go.clipX1();
-    }
-    if (yPos1 < go.clipY1()) {
-      ySrc1 += go.clipY1() - yPos1;
-      yPos1 = go.clipY1();
-    }
-    if (xPos2 >= go.clipX2()) {
-      xSrc2 -= xPos2 - go.clipX2();
-      xPos2 = go.clipX2();
-    }
-    if (yPos2 >= go.clipY2()) {
-      ySrc2 -= yPos2 - go.clipY2();
-      yPos2 = go.clipY2();
-    }
-  }
-
-  float fdx1 = xPos1, fdy1 = yPos1, fdx2 = xPos2, fdy2 = yPos2;
+  float fdx1 = dstRect.x(), fdy1 = dstRect.y(), fdx2 = dstRect.x2(),
+    fdy2 = dstRect.y2();
   if(!filterCoords(xSrc1, ySrc1, xSrc2, ySrc2,
                    fdx1, fdy1, fdx2, fdy2))
     return;

commit c384255964a1db118f0b294e5ee39e0735757f1c

Fixes regression where Yumemi's face is drawn *twice* during her final scene.
Reverts most of 65faeb2a9b4eb4970429592ec72b113a9328db46, which introduced
this bug. This has been broken for a long time...

src/Systems/Base/GraphicsObjectData.cpp
@@ -68,45 +68,17 @@ void GraphicsObjectData::render(const GraphicsObject& go, std::ostream* tree)
     Rect dst = dstRect(go);
     int alpha = getRenderingAlpha(go);
 
+    // TODO: Anyone attempting moving the clip area calculations here should
+    // verify that it doesn't break the final pan scene of Yumemi in
+    // Planetarian.
     if (tree) {
       objectInfo(*tree);
       *tree << "  Rendering " << src << " to " << dst;
       if (alpha != 255)
         *tree << " (alpha=" << alpha << ")";
+      if (go.hasClip())
+        *tree << " [Warning: Clip rectangle calculations not applied.]";
       *tree << endl;
-    }
-
-    // Perform the object clipping.
-    if (go.hasClip()) {
-      // Do nothing if object falls wholly outside clip area
-      if (dst.x2() < go.clipX1() || dst.x() > go.clipX2() ||
-          dst.y2() < go.clipY1() || dst.y() > go.clipY2())
-        return;
-
-      // Otherwise, adjust coordinates to present only the visible area.
-      if (dst.x() < go.clipX1()) {
-        src.setX( src.x() + go.clipX1() - dst.x());
-        dst.setX(go.clipX1());
-      }
-      if (dst.y() < go.clipY1()) {
-        src.setY(src.y() + go.clipY1() - dst.y());
-        dst.setY(go.clipY1());
-      }
-      if (dst.x2() >= go.clipX2()) {
-        src.setX2(src.x2() - dst.x2() + go.clipX2());
-        dst.setX2(go.clipX2());
-      }
-      if (dst.y2() >= go.clipY2()) {
-        src.setY2(src.y2() - dst.y2() + go.clipY2());
-        dst.setY2(go.clipY2());
-      }
-
-      if (tree) {
-        *tree << "  After clipping rect, " << src << " to " << dst;
-        if (alpha != 255)
-          *tree << " (alpha=" << alpha << ")";
-        *tree << endl;
-      }
     }
 
     surface->renderToScreenAsObject(go, src, dst, alpha);

src/Systems/Base/Rect.hpp
@@ -41,9 +41,7 @@ public:
   Point(int x, int y) : x_(x), y_(y) {}
 
   int x() const { return x_; }
-  void setX(const int in) { x_ = in; }
   int y() const { return y_; }
-  void setY(const int in) { y_ = in; }
 
   bool isEmpty() const { return x_ == 0 && y_ == 0; }
 
@@ -183,19 +181,15 @@ public:
   }
 
   int x() const { return origin_.x(); }
-  void setX(const int in) { origin_.setX(in); }
   int y() const { return origin_.y(); }
-  void setY(const int in) { origin_.setY(in); }
+  const Point& origin() const { return origin_; }
+
   int x2() const { return origin_.x() + size_.width(); }
-  void setX2(const int in) { size_.setWidth(in - origin_.x()); }
   int y2() const { return origin_.y() + size_.height(); }
-  void setY2(const int in) { size_.setHeight(in - origin_.y()); }
 
   int width() const { return size_.width(); }
   int height() const { return size_.height(); }
-
   const Size& size() const { return size_; }
-  const Point& origin() const { return origin_; }
 
   bool isEmpty() const { return origin_.isEmpty() && size_.isEmpty(); }
 

src/Systems/SDL/Texture.cpp
@@ -663,9 +663,40 @@ void Texture::renderToScreenAsObject(
   int ySrc1 = srcRect.y();
   int xSrc2 = srcRect.x2();
   int ySrc2 = srcRect.y2();
+  int xPos1 = dstRect.x();
+  int yPos1 = dstRect.y();
+  int xPos2 = dstRect.x2();
+  int yPos2 = dstRect.y2();
 
-  float fdx1 = dstRect.x(), fdy1 = dstRect.y(), fdx2 = dstRect.x2(),
-    fdy2 = dstRect.y2();
+  // If clipping is active for this object, take that into account too.
+  if (go.hasClip()) {
+    // Do nothing if object falls wholly outside clip area
+    if (xPos2 < go.clipX1() || xPos1 > go.clipX2() ||
+        yPos2 < go.clipY1() || yPos1 > go.clipY2()) {
+      return;
+    }
+    // Otherwise, adjust coordinates to present only the visible area.
+    // POINT
+    // TODO: Move this logic into an intersection of rectangles.
+    if (xPos1 < go.clipX1()) {
+      xSrc1 += go.clipX1() - xPos1;
+      xPos1 = go.clipX1();
+    }
+    if (yPos1 < go.clipY1()) {
+      ySrc1 += go.clipY1() - yPos1;
+      yPos1 = go.clipY1();
+    }
+    if (xPos2 >= go.clipX2()) {
+      xSrc2 -= xPos2 - go.clipX2();
+      xPos2 = go.clipX2();
+    }
+    if (yPos2 >= go.clipY2()) {
+      ySrc2 -= yPos2 - go.clipY2();
+      yPos2 = go.clipY2();
+    }
+  }
+
+  float fdx1 = xPos1, fdy1 = yPos1, fdx2 = xPos2, fdy2 = yPos2;
   if(!filterCoords(xSrc1, ySrc1, xSrc2, ySrc2,
                    fdx1, fdy1, fdx2, fdy2))
     return;