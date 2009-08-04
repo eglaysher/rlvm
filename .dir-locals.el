06f1c93f0653b7443e852790c3bf926775d8e068
Elliot Glaysher <glaysher@umich.edu>
2009-08-04 00:39:00 -0700

    Add local variables for emacs23.

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;; Project specific local variables for emacs23. `compile-command' is the only
+;; one not set by the individual files. Hopefully this list will grow.
+((nil . ((compile-command . "scons")
+         (tab-width . 2)
+         (indent-tabs-mode . nil))))
