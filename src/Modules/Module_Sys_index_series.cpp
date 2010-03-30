f071848ba06de6362fe62212e31aee0880bf8202
Elliot Glaysher <glaysher@umich.edu>
2010-03-29 23:26:06 -0700

    Attempt to fix index_series. Little Buster's battle scenes look better.
    - Approximate mode2. Haeleth described it as "a decelerating curve." While not
      equivalent, the look of the logarithmic curve I choose looks acceptable.
    - I am not sure about the implementation of mode1 though. As is, it appears to
      work, but I'm going to have to suspend judgment until the new object
      primitives in Little Busters are implemented.
    - The previous implementation had some weird problems with combining ranges.
      I'm not entirely sure that this is correct, though it is better?

--- src/Modules/Module_Sys_index_series.cpp
+++ src/Modules/Module_Sys_index_series.cpp
@@ -27,6 +27,10 @@
 
 #include "Modules/Module_Sys_index_series.hpp"
 
+#include <cmath>
+#include <sstream>
+#include <iostream>
+
 #include "MachineBase/RLMachine.hpp"
 #include "MachineBase/RLModule.hpp"
 #include "MachineBase/RLOperation.hpp"
@@ -64,19 +68,32 @@ struct Sys_index_series
           int end = it->second.get<1>();
           int endval = it->second.get<2>();
           mode0(index, start, end, endval, value, init);
-
           break;
         }
         case 2: {
           int start = it->third.get<0>();
           int end = it->third.get<1>();
           int endval = it->third.get<2>();
-          if (it->third.get<3>() == 0) {
-            mode0(index, start, end, endval, value, init);
-          } else {
-            throw rlvm::Exception(
-                "Don't know how to handle type 2 index_series statements");
+          if (index > start && index < end) {
+            if (it->third.get<3>() == 0) {
+              mode0(index, start, end, endval, value, init);
+            } else if (it->third.get<3>() == 1) {
+              mode1(index, start, end, endval, value, init);
+            } else if (it->third.get<3>() == 2) {
+              mode2(index, start, end, endval, value, init);
+            } else {
+              std::ostringstream oss;
+              oss << "Don't know how to handle type " << it->third.get<3>()
+                  << " index_series statements";
+              throw rlvm::Exception(oss.str());
+            }
+          } else if (index >= end) {
+            // Prevent us from going over the endval.
+            value = endval;
           }
+
+          init = endval;
+          break;
         }
       }
     }
@@ -85,15 +102,23 @@ struct Sys_index_series
   }
 
   void mode0(int index, int start, int end, int endval, int& value, int& init) {
-    if (index > start && index < end) {
-      double percentage = double(index - start) / double(end - start);
-      int amount = endval - init;
-      value = init + (percentage * amount);
-    } else if (index >= end) {
-      // Prevent us from going over the endval.
-      value = endval;
-      init = endval;
-    }
+    double percentage = double(index - start) / double(end - start);
+    int amount = endval - init;
+    value += (percentage * amount);
+  }
+
+  void mode1(int index, int start, int end, int endval, int& value, int& init) {
+    double percentage = double(index - start) / double(end - start);
+    double log_percentage = std::log(percentage + 1) / std::log(2);
+    int amount = endval - init;
+    value += (amount - ((1 - log_percentage) * amount));
+  }
+
+  void mode2(int index, int start, int end, int endval, int& value, int& init) {
+    double percentage = double(index - start) / double(end - start);
+    double log_percentage = std::log(percentage + 1) / std::log(2);
+    int amount = endval - init;
+    value += (log_percentage * amount);
   }
 };
 
