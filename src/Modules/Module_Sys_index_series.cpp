5b71d4b78cac130d9bd19d1e21987c07dc92b388
Elliot Glaysher <glaysher@umich.edu>
2010-09-19 15:54:52 -0700

    Fix regression in index_series.
    In f071848ba06de6362fe62212e31aee0880bf8202, I tried to lift a common if check
    out of each modeX() method. This was subtly wrong.
    
    This fixes the regression in Kanon's scrolling text before every day, AND
    makes Little Buster's battle introductions look even smoother.

--- src/Modules/Module_Sys_index_series.cpp
+++ src/Modules/Module_Sys_index_series.cpp
@@ -74,25 +74,18 @@ struct Sys_index_series
           int start = it->third.get<0>();
           int end = it->third.get<1>();
           int endval = it->third.get<2>();
-          if (index > start && index < end) {
-            if (it->third.get<3>() == 0) {
-              mode0(index, start, end, endval, value, init);
-            } else if (it->third.get<3>() == 1) {
-              mode1(index, start, end, endval, value, init);
-            } else if (it->third.get<3>() == 2) {
-              mode2(index, start, end, endval, value, init);
-            } else {
-              std::ostringstream oss;
-              oss << "Don't know how to handle type " << it->third.get<3>()
-                  << " index_series statements";
-              throw rlvm::Exception(oss.str());
-            }
-          } else if (index >= end) {
-            // Prevent us from going over the endval.
-            value = endval;
+          if (it->third.get<3>() == 0) {
+            mode0(index, start, end, endval, value, init);
+          } else if (it->third.get<3>() == 1) {
+            mode1(index, start, end, endval, value, init);
+          } else if (it->third.get<3>() == 2) {
+            mode2(index, start, end, endval, value, init);
+          } else {
+            std::ostringstream oss;
+            oss << "Don't know how to handle type " << it->third.get<3>()
+                << " index_series statements";
+            throw rlvm::Exception(oss.str());
           }
-
-          init = endval;
           break;
         }
       }
@@ -102,23 +95,41 @@ struct Sys_index_series
   }
 
   void mode0(int index, int start, int end, int endval, int& value, int& init) {
-    double percentage = double(index - start) / double(end - start);
-    int amount = endval - init;
-    value += (percentage * amount);
+    if (index > start && index < end) {
+      double percentage = double(index - start) / double(end - start);
+      int amount = endval - init;
+      value += (percentage * amount);
+    } else if (index >= end) {
+      // Prevent us from going over the endval.
+      value = endval;
+      init = endval;
+    }
   }
 
   void mode1(int index, int start, int end, int endval, int& value, int& init) {
-    double percentage = double(index - start) / double(end - start);
-    double log_percentage = std::log(percentage + 1) / std::log(2);
-    int amount = endval - init;
-    value += (amount - ((1 - log_percentage) * amount));
+    if (index > start && index < end) {
+      double percentage = double(index - start) / double(end - start);
+      double log_percentage = std::log(percentage + 1) / std::log(2);
+      int amount = endval - init;
+      value += (amount - ((1 - log_percentage) * amount));
+    } else if (index >= end) {
+      // Prevent us from going over the endval.
+      value = endval;
+      init = endval;
+    }
   }
 
   void mode2(int index, int start, int end, int endval, int& value, int& init) {
-    double percentage = double(index - start) / double(end - start);
-    double log_percentage = std::log(percentage + 1) / std::log(2);
-    int amount = endval - init;
-    value += (log_percentage * amount);
+    if (index > start && index < end) {
+      double percentage = double(index - start) / double(end - start);
+      double log_percentage = std::log(percentage + 1) / std::log(2);
+      int amount = endval - init;
+      value += (log_percentage * amount);
+    } else if (index >= end) {
+      // Prevent us from going over the endval.
+      value = endval;
+      init = endval;
+    }
   }
 };
 
