eglaysher/rlvm

commit c7863df04248097762f7614125bb592cb9ac7c2d

Refactor Colour Filters into their own file.

src/Systems/Base/ColourFilter.hpp
@@ -1,0 +1,41 @@
+// -*- Mode: C++; tab-width:2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi:tw=80:et:ts=2:sts=2
+//
+// -----------------------------------------------------------------------
+//
+// This file is part of RLVM, a RealLive virtual machine clone.
+//
+// -----------------------------------------------------------------------
+//
+// Copyright (C) 2013 Elliot Glaysher
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+// -----------------------------------------------------------------------
+
+#ifndef SRC_SYSTEMS_BASE_COLOURFILTER_HPP_
+#define SRC_SYSTEMS_BASE_COLOURFILTER_HPP_
+
+class RGBAColour;
+
+// An interface for platform specific colour filters.
+class ColourFilter {
+ public:
+  virtual ~ColourFilter() {}
+
+  // Fills an area of the screen with |colour|.
+  virtual void Fill(const RGBAColour& colour) = 0;
+};
+
+#endif  // SRC_SYSTEMS_BASE_COLOURFILTER_HPP_

src/Systems/Base/ColourFilterObjectData.cpp
@@ -35,6 +35,7 @@
 using namespace std;
 
 #include "Systems/Base/Colour.hpp"
+#include "Systems/Base/ColourFilter.hpp"
 #include "Systems/Base/GraphicsObject.hpp"
 #include "Systems/Base/GraphicsSystem.hpp"
 #include "Systems/Base/System.hpp"
@@ -51,24 +52,28 @@ ColourFilterObjectData::~ColourFilterObjectData() {}
 void ColourFilterObjectData::render(const GraphicsObject& go,
                                     const GraphicsObject* parent,
                                     std::ostream* tree) {
-  if (go.mono() == 0) {
-    RGBAColour colour = go.colour();
-    colour.setAlpha(
-        static_cast<int>(colour.a_float() * go.computedAlpha()));
-
-    graphics_system_.fillScreenArea(screen_rect_, colour);
-
-    if (tree) {
-      *tree << "  ColourFilterObjectData" << std::endl
-            << "  Screen rect: " << screen_rect_ << std::endl
-            << "  Colour: " << colour << std::endl;
-    }
-  } else {
+  if (go.width() != 100 || go.height() != 100) {
     static bool printed = false;
     if (!printed) {
       printed = true;
-      cerr << "We don't yet deal with objMono() and colour filters." << endl;
+      cerr << "We can't yet scaling colour filters." << endl;
     }
+  }
+
+  // Lazily create colour object.
+  if (!colour_filer_) {
+    colour_filer_.reset(graphics_system_.BuildColourFiller(screen_rect_));
+  }
+
+  RGBAColour colour = go.colour();
+  colour.setAlpha(
+      static_cast<int>(colour.a_float() * go.computedAlpha()));
+  colour_filer_->Fill(colour);
+
+  if (tree) {
+    *tree << "  ColourFilterObjectData" << std::endl
+          << "  Screen rect: " << screen_rect_ << std::endl
+          << "  Colour: " << colour << std::endl;
   }
 }
 

src/Systems/Base/ColourFilterObjectData.hpp
@@ -35,6 +35,7 @@
 #include "MachineBase/RLMachine.hpp"
 #include "MachineBase/Serialization.hpp"
 
+class ColourFilter;
 class GraphicsObject;
 class GraphicsSystem;
 
@@ -68,6 +69,8 @@ class ColourFilterObjectData : public GraphicsObjectData {
   GraphicsSystem& graphics_system_;
 
   Rect screen_rect_;
+
+  boost::scoped_ptr<ColourFilter> colour_filer_;
 
   friend class boost::serialization::access;
   template<class Archive>

src/Systems/Base/GraphicsSystem.hpp
@@ -49,6 +49,7 @@
 #include "Utilities/LazyArray.hpp"
 #include "lru_cache.hpp"
 
+class ColourFilter;
 class Gameexe;
 class GraphicsObject;
 class GraphicsObjectData;
@@ -365,7 +366,7 @@ class GraphicsSystem : public EventListener {
 
   virtual boost::shared_ptr<Surface> buildSurface(const Size& size) = 0;
 
-  virtual void fillScreenArea(const Rect& rect, const RGBAColour& colour) = 0;
+  virtual ColourFilter* BuildColourFiller(const Rect& rect) = 0;
 
   // Clears and promotes objects.
   void clearAndPromoteObjects();

src/Systems/SDL/SDLColourFilter.cpp
@@ -1,0 +1,56 @@
+// -*- Mode: C++; tab-width:2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi:tw=80:et:ts=2:sts=2
+//
+// -----------------------------------------------------------------------
+//
+// This file is part of RLVM, a RealLive virtual machine clone.
+//
+// -----------------------------------------------------------------------
+//
+// Copyright (C) 2013 Elliot Glaysher
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+// -----------------------------------------------------------------------
+
+#include "Systems/SDL/SDLColourFilter.hpp"
+
+#include "Systems/SDL/SDLUtils.hpp"
+
+SDLColourFilter::SDLColourFilter(const Rect& screen_rect)
+    : screen_rect_(screen_rect),
+      back_texture_id_(0) {
+}
+
+SDLColourFilter::~SDLColourFilter() {}
+
+void SDLColourFilter::Fill(const RGBAColour& colour) {
+  glDisable(GL_TEXTURE_2D);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+  glColorRGBA(colour);
+
+  glBegin(GL_QUADS);
+  {
+    glVertex2f(screen_rect_.x(), screen_rect_.y());
+    glVertex2f(screen_rect_.x(), screen_rect_.y() + screen_rect_.height());
+    glVertex2f(screen_rect_.x() + screen_rect_.width(),
+               screen_rect_.y() + screen_rect_.height());
+    glVertex2f(screen_rect_.x() + screen_rect_.width(), screen_rect_.y());
+  }
+  glEnd();
+
+  glEnable(GL_TEXTURE_2D);
+  glBlendFunc(GL_ONE, GL_ZERO);
+}
+

src/Systems/SDL/SDLGraphicsSystem.cpp
@@ -58,6 +58,7 @@
 #include "Systems/Base/SystemError.hpp"
 #include "Systems/Base/TextSystem.hpp"
 #include "Systems/Base/ToneCurve.hpp"
+#include "Systems/SDL/SDLColourFilter.hpp"
 #include "Systems/SDL/SDLEventSystem.hpp"
 #include "Systems/SDL/SDLRenderToTextureSurface.hpp"
 #include "Systems/SDL/SDLSurface.hpp"
@@ -654,24 +655,8 @@ boost::shared_ptr<Surface> SDLGraphicsSystem::buildSurface(const Size& size) {
   return shared_ptr<Surface>(new SDLSurface(this, size));
 }
 
-void SDLGraphicsSystem::fillScreenArea(const Rect& rect,
-                                       const RGBAColour& colour) {
-  if (colour.a()) {
-    glDisable(GL_TEXTURE_2D);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-    glBegin(GL_QUADS); {
-      glColorRGBA(colour);
-      glVertex2f(rect.x(), rect.y());
-      glVertex2f(rect.x(), rect.y() + rect.height());
-      glVertex2f(rect.x() + rect.width(), rect.y() + rect.height());
-      glVertex2f(rect.x() + rect.width(), rect.y());
-    }
-    glEnd();
-
-    glEnable(GL_TEXTURE_2D);
-    glBlendFunc(GL_ONE, GL_ZERO);
-  }
+ColourFilter* SDLGraphicsSystem::BuildColourFiller(const Rect& rect) {
+  return new SDLColourFilter(rect);
 }
 
 void SDLGraphicsSystem::reset() {

src/Systems/SDL/SDLGraphicsSystem.hpp
@@ -89,7 +89,8 @@ class SDLGraphicsSystem : public GraphicsSystem {
   virtual boost::shared_ptr<Surface> getHaikei();
   virtual boost::shared_ptr<Surface> getDC(int dc);
   virtual boost::shared_ptr<Surface> buildSurface(const Size& size);
-  virtual void fillScreenArea(const Rect& rect, const RGBAColour& colour);
+
+  virtual ColourFilter* BuildColourFiller(const Rect& rect);
 
   // -----------------------------------------------------------------------
 

test/TestSystem/TestGraphicsSystem.cpp
@@ -132,8 +132,8 @@ boost::shared_ptr<Surface> TestGraphicsSystem::buildSurface(const Size& s) {
   return boost::shared_ptr<Surface>(MockSurface::Create(oss.str(), s));
 }
 
-void TestGraphicsSystem::fillScreenArea(const Rect& rect,
-                                        const RGBAColour& colour) {
+ColourFilter* TestGraphicsSystem::BuildColourFiller(const Rect& rect) {
+  return NULL;
 }
 
 void TestGraphicsSystem::blitSurfaceToDC(

commit 28a5550965edd58b11529039ea3936ee38ef9e5b

The colour filter object area should be passed each time it renders.
When creating the current ColourFilter interface, I made the area where
the filter was rendered set at construction time. This meant that
further updates to the rect were ignored. Now we pass the current rect
each time we try to render. Added unit test so this shouldn't regress.

Fixes regression introduced in c7863df04248097762f7614125bb592cb9ac7c2d.

src/Systems/Base/ColourFilter.hpp
@@ -28,6 +28,7 @@
 #define SRC_SYSTEMS_BASE_COLOURFILTER_HPP_
 
 class GraphicsObject;
+class Rect;
 class RGBAColour;
 
 // An interface for platform specific colour filters.
@@ -37,6 +38,7 @@ class ColourFilter {
 
   // Fills an area of the screen with |colour|.
   virtual void Fill(const GraphicsObject& go,
+                    const Rect& screen_rect,
                     const RGBAColour& colour) = 0;
 };
 

src/Systems/Base/ColourFilterObjectData.cpp
@@ -49,6 +49,12 @@ ColourFilterObjectData::ColourFilterObjectData(GraphicsSystem& system,
 
 ColourFilterObjectData::~ColourFilterObjectData() {}
 
+ColourFilter* ColourFilterObjectData::GetColourFilter() {
+  if (!colour_filer_)
+    colour_filer_.reset(graphics_system_.BuildColourFiller());
+  return colour_filer_.get();
+}
+
 void ColourFilterObjectData::render(const GraphicsObject& go,
                                     const GraphicsObject* parent,
                                     std::ostream* tree) {
@@ -60,13 +66,8 @@ void ColourFilterObjectData::render(const GraphicsObject& go,
     }
   }
 
-  // Lazily create colour object.
-  if (!colour_filer_) {
-    colour_filer_.reset(graphics_system_.BuildColourFiller(screen_rect_));
-  }
-
   RGBAColour colour = go.colour();
-  colour_filer_->Fill(go, colour);
+  GetColourFilter()->Fill(go, screen_rect_, colour);
 
   if (tree) {
     *tree << "  ColourFilterObjectData" << std::endl

src/Systems/Base/ColourFilterObjectData.hpp
@@ -49,6 +49,9 @@ class ColourFilterObjectData : public GraphicsObjectData {
 
   void setRect(const Rect& screen_rect) { screen_rect_ = screen_rect; }
 
+  // Returns the colour filter, lazily creating it if necessary.
+  ColourFilter* GetColourFilter();
+
   // Overriden from GraphicsObjectData:
   virtual void render(const GraphicsObject& go,
                       const GraphicsObject* parent,

src/Systems/Base/GraphicsSystem.hpp
@@ -366,7 +366,7 @@ class GraphicsSystem : public EventListener {
 
   virtual boost::shared_ptr<Surface> buildSurface(const Size& size) = 0;
 
-  virtual ColourFilter* BuildColourFiller(const Rect& rect) = 0;
+  virtual ColourFilter* BuildColourFiller() = 0;
 
   // Clears and promotes objects.
   void clearAndPromoteObjects();

src/Systems/SDL/SDLColourFilter.cpp
@@ -34,9 +34,8 @@
 #include "Systems/SDL/Shaders.hpp"
 #include "Systems/SDL/Texture.hpp"
 
-SDLColourFilter::SDLColourFilter(const Rect& screen_rect)
-    : screen_rect_(screen_rect),
-      texture_width_(0),
+SDLColourFilter::SDLColourFilter()
+    : texture_width_(0),
       texture_height_(0),
       back_texture_id_(0) {
 }
@@ -47,6 +46,7 @@ SDLColourFilter::~SDLColourFilter() {
 }
 
 void SDLColourFilter::Fill(const GraphicsObject& go,
+                           const Rect& screen_rect,
                            const RGBAColour& colour) {
   if (GLEW_ARB_fragment_shader && GLEW_ARB_multitexture) {
     if (back_texture_id_ == 0) {
@@ -56,8 +56,8 @@ void SDLColourFilter::Fill(const GraphicsObject& go,
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-      texture_width_ = SafeSize(screen_rect_.width());
-      texture_height_ = SafeSize(screen_rect_.height());
+      texture_width_ = SafeSize(screen_rect.width());
+      texture_height_ = SafeSize(screen_rect.height());
       glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA,
                    texture_width_, texture_height_,
                    0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
@@ -67,9 +67,9 @@ void SDLColourFilter::Fill(const GraphicsObject& go,
     // Copy the current value of the region where we're going to render
     // to a texture for input to the shader
     glBindTexture(GL_TEXTURE_2D, back_texture_id_);
-    int ystart = int(Texture::ScreenHeight() - screen_rect_.y() -
-                     screen_rect_.height());
-    int idx1 = screen_rect_.x();
+    int ystart = int(Texture::ScreenHeight() - screen_rect.y() -
+                     screen_rect.height());
+    int idx1 = screen_rect.x();
     glCopyTexSubImage2D(GL_TEXTURE_2D,
                         0,
                         0, 0,
@@ -85,21 +85,21 @@ void SDLColourFilter::Fill(const GraphicsObject& go,
 
     float thisx1 = 0;
     float thisy1 = 0;
-    float thisx2 = float(screen_rect_.width()) / texture_width_;
-    float thisy2 = float(screen_rect_.height()) / texture_height_;
+    float thisx2 = float(screen_rect.width()) / texture_width_;
+    float thisy2 = float(screen_rect.height()) / texture_height_;
 
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glBegin(GL_QUADS);
     {
       glTexCoord2f(thisx1, thisy2);
-      glVertex2f(screen_rect_.x(), screen_rect_.y());
+      glVertex2f(screen_rect.x(), screen_rect.y());
       glTexCoord2f(thisx1, thisy1);
-      glVertex2f(screen_rect_.x(), screen_rect_.y() + screen_rect_.height());
+      glVertex2f(screen_rect.x(), screen_rect.y() + screen_rect.height());
       glTexCoord2f(thisx2, thisy1);
-      glVertex2f(screen_rect_.x() + screen_rect_.width(),
-                 screen_rect_.y() + screen_rect_.height());
+      glVertex2f(screen_rect.x() + screen_rect.width(),
+                 screen_rect.y() + screen_rect.height());
       glTexCoord2f(thisx2, thisy2);
-      glVertex2f(screen_rect_.x() + screen_rect_.width(), screen_rect_.y());
+      glVertex2f(screen_rect.x() + screen_rect.width(), screen_rect.y());
     }
     glEnd();
     glBlendFunc(GL_ONE, GL_ZERO);

src/Systems/SDL/SDLGraphicsSystem.cpp
@@ -655,8 +655,8 @@ boost::shared_ptr<Surface> SDLGraphicsSystem::buildSurface(const Size& size) {
   return shared_ptr<Surface>(new SDLSurface(this, size));
 }
 
-ColourFilter* SDLGraphicsSystem::BuildColourFiller(const Rect& rect) {
-  return new SDLColourFilter(rect);
+ColourFilter* SDLGraphicsSystem::BuildColourFiller() {
+  return new SDLColourFilter();
 }
 
 void SDLGraphicsSystem::reset() {

src/Systems/SDL/SDLGraphicsSystem.hpp
@@ -90,7 +90,7 @@ class SDLGraphicsSystem : public GraphicsSystem {
   virtual boost::shared_ptr<Surface> getDC(int dc);
   virtual boost::shared_ptr<Surface> buildSurface(const Size& size);
 
-  virtual ColourFilter* BuildColourFiller(const Rect& rect);
+  virtual ColourFilter* BuildColourFiller();
 
   // -----------------------------------------------------------------------
 

test/TestSystem/MockColourFilter.hpp
@@ -24,32 +24,19 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 // -----------------------------------------------------------------------
 
-#ifndef SRC_SYSTEMS_SDL_SDLCOLOURFILTER_HPP_
-#define SRC_SYSTEMS_SDL_SDLCOLOURFILTER_HPP_
+#ifndef TEST_TESTSYSTEM_MOCKCOLOURFILTER_HPP_
+#define TEST_TESTSYSTEM_MOCKCOLOURFILTER_HPP_
 
-#include <SDL/SDL_opengl.h>
+#include "gmock/gmock.h"
 
 #include "Systems/Base/ColourFilter.hpp"
-#include "Systems/Base/Rect.hpp"
 
-class RGBAColour;
-
-// OpenGL specific implementation of a ColourFilter.
-class SDLColourFilter : public ColourFilter {
+class MockColourFilter : public ColourFilter {
  public:
-  SDLColourFilter();
-  virtual ~SDLColourFilter();
-
-  // Overriden from ColourFilter:
-  virtual void Fill(const GraphicsObject& go,
-                    const Rect& screen_rect,
-                    const RGBAColour& colour);
-
- private:
-  unsigned int texture_width_;
-  unsigned int texture_height_;
-
-  GLuint back_texture_id_;
+  MOCK_METHOD3(Fill, void(const GraphicsObject&,
+                          const Rect&,
+                          const RGBAColour&));
 };
 
-#endif  // SRC_SYSTEMS_SDL_SDLCOLOURFILTER_HPP_
+
+#endif  // TEST_TESTSYSTEM_MOCKCOLOURFILTER_HPP_

test/TestSystem/TestGraphicsSystem.cpp
@@ -30,6 +30,7 @@
 #include "Systems/Base/Colour.hpp"
 #include "Systems/Base/GraphicsObject.hpp"
 #include "Systems/Base/GraphicsSystem.hpp"
+#include "TestSystem/MockColourFilter.hpp"
 #include "TestSystem/MockSurface.hpp"
 #include "Utilities/Exception.hpp"
 
@@ -40,7 +41,7 @@
 using namespace std;
 
 TestGraphicsSystem::TestGraphicsSystem(System& system, Gameexe& gexe)
-  : GraphicsSystem(system, gexe) {
+    : GraphicsSystem(system, gexe) {
   for (int i = 0; i < 16; ++i) {
     ostringstream oss;
     oss << "DC #" << i;
@@ -53,6 +54,8 @@ TestGraphicsSystem::TestGraphicsSystem(System& system, Gameexe& gexe)
   haikei_.reset(MockSurface::Create("Haikei"));
   haikei_->allocate(screenSize());
 }
+
+TestGraphicsSystem::~TestGraphicsSystem() {}
 
 void TestGraphicsSystem::allocateDC(int dc, Size size) {
   if (dc >= 16)
@@ -132,8 +135,8 @@ boost::shared_ptr<Surface> TestGraphicsSystem::buildSurface(const Size& s) {
   return boost::shared_ptr<Surface>(MockSurface::Create(oss.str(), s));
 }
 
-ColourFilter* TestGraphicsSystem::BuildColourFiller(const Rect& rect) {
-  return NULL;
+ColourFilter* TestGraphicsSystem::BuildColourFiller() {
+  return new MockColourFilter;
 }
 
 void TestGraphicsSystem::blitSurfaceToDC(

test/TestSystem/TestGraphicsSystem.hpp
@@ -38,10 +38,12 @@
 
 class System;
 class MockSurface;
+class MockColourFilter;
 
 class TestGraphicsSystem : public GraphicsSystem {
  public:
   TestGraphicsSystem(System& system, Gameexe& gexe);
+  virtual ~TestGraphicsSystem();
 
   virtual void executeGraphicsSystem(RLMachine& machine) {
     GraphicsSystem::executeGraphicsSystem(machine);
@@ -68,7 +70,7 @@ class TestGraphicsSystem : public GraphicsSystem {
   virtual boost::shared_ptr<Surface> getHaikei();
   virtual boost::shared_ptr<Surface> getDC(int dc);
   virtual boost::shared_ptr<Surface> buildSurface(const Size& s);
-  virtual ColourFilter* BuildColourFiller(const Rect& rect);
+  virtual ColourFilter* BuildColourFiller();
 
   virtual void blitSurfaceToDC(
       Surface& source_obj, int target_dc,

test/graphics_object_test.cpp
@@ -41,8 +41,10 @@
 #include "MachineBase/RLMachine.hpp"
 #include "MachineBase/Serialization.hpp"
 #include "Modules/Module_Str.hpp"
+#include "TestSystem/MockColourFilter.hpp"
 #include "TestSystem/TestGraphicsSystem.hpp"
 #include "TestSystem/TestSystem.hpp"
+#include "Systems/Base/ColourFilterObjectData.hpp"
 #include "Systems/Base/GraphicsObject.hpp"
 #include "Systems/Base/GraphicsObjectOfFile.hpp"
 #include "Utilities/Exception.hpp"
@@ -60,6 +62,7 @@ using namespace std;
 using namespace libReallive;
 using namespace Serialization;
 
+using ::testing::_;
 using ::testing::Ref;
 using ::testing::Return;
 
@@ -251,3 +254,32 @@ TEST_F(GraphicsObjectTest, GetObjectData) {
 }
 
 // TODO: Use the above mock to test more of the insides of GraphicsObject...
+
+
+TEST_F(GraphicsObjectTest, TestColourFilter) {
+  // In the past, we've had regressions because we ignored updated screen_rects
+  // in colour filter objects.
+
+  GraphicsObject obj;
+
+  // Two completely different rects.
+  const Rect one(10, 12, Size(18, 14));
+  const Rect two(43, 81, Size(5, 20));
+
+  ColourFilterObjectData* data =
+      new ColourFilterObjectData(system.graphics(), one);
+  obj.setObjectData(data);
+
+  MockColourFilter* filter =
+      dynamic_cast<MockColourFilter*>(data->GetColourFilter());
+  EXPECT_CALL(*filter, Fill(_, one, _));
+  EXPECT_CALL(*filter, Fill(_, two, _));
+
+  // Render as is (for the first call).
+  data->render(obj, NULL, NULL);
+
+  data->setRect(two);
+
+  // Render with the modified rect (for the second call).
+  data->render(obj, NULL, NULL);
+}