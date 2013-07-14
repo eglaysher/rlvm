eglaysher/rlvm

commit be82c2cb8bb38a6ccb833978e89110b7326a4cc6

Implement color filters using the object shader
This means we now render the sepia scenes in CLANNAD correctly, but only when
we have shaders available. I'm completely cutting the fallback since it renders
incorrectly in most cases and doing nothing is preferable. This also fixes the
transition into the LB Refrain ending.

Closes GH-14

src/Systems/Base/ColourFilterObjectData.cpp
@@ -68,7 +68,7 @@ void ColourFilterObjectData::render(const GraphicsObject& go,
   RGBAColour colour = go.colour();
   colour.setAlpha(
       static_cast<int>(colour.a_float() * go.computedAlpha()));
-  colour_filer_->Fill(colour);
+  colour_filer_->Fill(go, colour);
 
   if (tree) {
     *tree << "  ColourFilterObjectData" << std::endl

src/Systems/SDL/SDLColourFilter.cpp
@@ -24,33 +24,90 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 // -----------------------------------------------------------------------
 
+#include "GL/glew.h"
+
 #include "Systems/SDL/SDLColourFilter.hpp"
 
+#include "Systems/Base/Colour.hpp"
+#include "Systems/Base/GraphicsObject.hpp"
 #include "Systems/SDL/SDLUtils.hpp"
+#include "Systems/SDL/Shaders.hpp"
+#include "Systems/SDL/Texture.hpp"
 
 SDLColourFilter::SDLColourFilter(const Rect& screen_rect)
     : screen_rect_(screen_rect),
+      texture_width_(0),
+      texture_height_(0),
       back_texture_id_(0) {
 }
 
-SDLColourFilter::~SDLColourFilter() {}
+SDLColourFilter::~SDLColourFilter() {
+  if (back_texture_id_)
+    glDeleteTextures(1, &back_texture_id_);
+}
 
-void SDLColourFilter::Fill(const RGBAColour& colour) {
-  glDisable(GL_TEXTURE_2D);
-  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-  glColorRGBA(colour);
+void SDLColourFilter::Fill(const GraphicsObject& go,
+                           const RGBAColour& colour) {
+  if (GLEW_ARB_fragment_shader && GLEW_ARB_multitexture) {
+    if (back_texture_id_ == 0) {
+      glGenTextures(1, &back_texture_id_);
+      glBindTexture(GL_TEXTURE_2D, back_texture_id_);
+      DebugShowGLErrors();
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-  glBegin(GL_QUADS);
-  {
-    glVertex2f(screen_rect_.x(), screen_rect_.y());
-    glVertex2f(screen_rect_.x(), screen_rect_.y() + screen_rect_.height());
-    glVertex2f(screen_rect_.x() + screen_rect_.width(),
-               screen_rect_.y() + screen_rect_.height());
-    glVertex2f(screen_rect_.x() + screen_rect_.width(), screen_rect_.y());
+      texture_width_ = SafeSize(screen_rect_.width());
+      texture_height_ = SafeSize(screen_rect_.height());
+      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA,
+                   texture_width_, texture_height_,
+                   0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+      DebugShowGLErrors();
+    }
+
+    // Copy the current value of the region where we're going to render
+    // to a texture for input to the shader
+    glBindTexture(GL_TEXTURE_2D, back_texture_id_);
+    int ystart = int(Texture::ScreenHeight() - screen_rect_.y() -
+                     screen_rect_.height());
+    int idx1 = screen_rect_.x();
+    glCopyTexSubImage2D(GL_TEXTURE_2D,
+                        0,
+                        0, 0,
+                        idx1, ystart, texture_width_, texture_height_);
+    DebugShowGLErrors();
+
+    // Set up shader
+    glUseProgramObjectARB(Shaders::getObjectProgram());
+    glUniform1iARB(Shaders::getObjectUniformImage(), 0);
+    // The alpha is 255 because we are replacing the image on the screen
+    // instead of blending with it.
+    glUniform1fARB(Shaders::getObjectUniformAlpha(), 1.0f);
+    Shaders::loadObjectUniformFromGraphicsObject(go);
+
+    float thisx1 = 0;
+    float thisy1 = 0;
+    float thisx2 = float(screen_rect_.width()) / texture_width_;
+    float thisy2 = float(screen_rect_.height()) / texture_height_;
+
+    // Don't blend with the background. Blit on top of it.
+    glDisable(GL_BLEND);
+
+    glBegin(GL_QUADS);
+    {
+      glTexCoord2f(thisx1, thisy2);
+      glVertex2f(screen_rect_.x(), screen_rect_.y());
+      glTexCoord2f(thisx1, thisy1);
+      glVertex2f(screen_rect_.x(), screen_rect_.y() + screen_rect_.height());
+      glTexCoord2f(thisx2, thisy1);
+      glVertex2f(screen_rect_.x() + screen_rect_.width(),
+                 screen_rect_.y() + screen_rect_.height());
+      glTexCoord2f(thisx2, thisy2);
+      glVertex2f(screen_rect_.x() + screen_rect_.width(), screen_rect_.y());
+    }
+    glEnd();
+
+    glEnable(GL_BLEND);
+    glUseProgramObjectARB(0);
   }
-  glEnd();
-
-  glEnable(GL_TEXTURE_2D);
-  glBlendFunc(GL_ONE, GL_ZERO);
 }
 

src/Systems/Base/ColourFilter.hpp
@@ -27,6 +27,7 @@
 #ifndef SRC_SYSTEMS_BASE_COLOURFILTER_HPP_
 #define SRC_SYSTEMS_BASE_COLOURFILTER_HPP_
 
+class GraphicsObject;
 class RGBAColour;
 
 // An interface for platform specific colour filters.
@@ -35,7 +36,8 @@ class ColourFilter {
   virtual ~ColourFilter() {}
 
   // Fills an area of the screen with |colour|.
-  virtual void Fill(const RGBAColour& colour) = 0;
+  virtual void Fill(const GraphicsObject& go,
+                    const RGBAColour& colour) = 0;
 };
 
 #endif  // SRC_SYSTEMS_BASE_COLOURFILTER_HPP_

src/Systems/SDL/SDLColourFilter.hpp
@@ -41,12 +41,16 @@ class SDLColourFilter : public ColourFilter {
   virtual ~SDLColourFilter();
 
   // Overriden from ColourFilter:
-  virtual void Fill(const RGBAColour& colour);
+  virtual void Fill(const GraphicsObject& go,
+                    const RGBAColour& colour);
 
  private:
   Rect screen_rect_;
 
-  GLint back_texture_id_;
+  unsigned int texture_width_;
+  unsigned int texture_height_;
+
+  GLuint back_texture_id_;
 };
 
 #endif  // SRC_SYSTEMS_SDL_SDLCOLOURFILTER_HPP_

src/Systems/SDL/Shaders.cpp
@@ -28,6 +28,7 @@
 
 #include <iostream>
 
+#include "Systems/Base/GraphicsObject.hpp"
 #include "Systems/Base/SystemError.hpp"
 #include "Systems/SDL/SDLUtils.hpp"
 #include "Systems/SDL/Shaders.hpp"
@@ -172,6 +173,26 @@ GLint Shaders::getObjectUniformImage() {
   }
 
   return object_image_;
+}
+
+void Shaders::loadObjectUniformFromGraphicsObject(const GraphicsObject& go) {
+  RGBAColour colour = go.colour();
+  glUniform4fARB(Shaders::getObjectUniformColour(),
+                 colour.r_float(), colour.g_float(),
+                 colour.b_float(), colour.a_float());
+
+  RGBColour tint = go.tint();
+  glUniform3fARB(Shaders::getObjectUniformTint(),
+                 tint.r_float(), tint.g_float(), tint.b_float());
+
+  glUniform1fARB(Shaders::getObjectUniformLight(),
+                 go.light() / 255.0f);
+
+  glUniform1fARB(Shaders::getObjectUniformMono(),
+                 go.mono() / 255.0f);
+
+  glUniform1fARB(Shaders::getObjectUniformInvert(),
+                 go.invert() / 255.0f);
 }
 
 GLint Shaders::getObjectUniformColour() {

src/Systems/SDL/Shaders.hpp
@@ -29,6 +29,8 @@
 
 #include <SDL/SDL_opengl.h>
 
+class GraphicsObject;
+
 // Static state about shaders. We just leak them.
 class Shaders {
  public:
@@ -44,10 +46,14 @@ class Shaders {
 
   // Returns the parameters to the object program.
   static GLint getObjectUniformImage();
+  static GLint getObjectUniformAlpha();
+
+  // Set the colour/tint/light/mono/invert properties from |go|. (Individual
+  // setters also exposed.)
+  static void loadObjectUniformFromGraphicsObject(const GraphicsObject& go);
   static GLint getObjectUniformColour();
   static GLint getObjectUniformTint();
   static GLint getObjectUniformLight();
-  static GLint getObjectUniformAlpha();
   static GLint getObjectUniformMono();
   static GLint getObjectUniformInvert();
 

src/Systems/SDL/Texture.cpp
@@ -637,27 +637,12 @@ void Texture::renderToScreenAsObject(
       glUseProgramObjectARB(Shaders::getObjectProgram());
       glUniform1iARB(Shaders::getObjectUniformImage(), 0);
 
-      // Colour
-      RGBAColour colour = go.colour();
-      glUniform4fARB(Shaders::getObjectUniformColour(),
-                     colour.r_float(), colour.g_float(),
-                     colour.b_float(), colour.a_float());
+      // Colour/Tint/Etc.
+      Shaders::loadObjectUniformFromGraphicsObject(go);
 
-      RGBColour tint = go.tint();
-      glUniform3fARB(Shaders::getObjectUniformTint(),
-                     tint.r_float(), tint.g_float(), tint.b_float());
-
-      glUniform1fARB(Shaders::getObjectUniformLight(),
-                     go.light() / 255.0f);
-
+      // Alpha.
       glUniform1fARB(Shaders::getObjectUniformAlpha(),
                      alpha / 255.0f);
-
-      glUniform1fARB(Shaders::getObjectUniformMono(),
-                     go.mono() / 255.0f);
-
-      glUniform1fARB(Shaders::getObjectUniformInvert(),
-                     go.invert() / 255.0f);
 
       // Our final blending color has to be all white here.
       using_shader = true;

commit ff24b46d2d6f84a899f5c993d056299e9afedb22

Fix colour objects ignoring their alpha properties.
Alpha was computed in the wrong place. This also changes the clear color
so that we're using opaque black instead of transparent black.

This broke in be82c2cb8bb38a6ccb833978e89110b7326a4cc6.

src/Systems/Base/ColourFilterObjectData.cpp
@@ -66,8 +66,6 @@ void ColourFilterObjectData::render(const GraphicsObject& go,
   }
 
   RGBAColour colour = go.colour();
-  colour.setAlpha(
-      static_cast<int>(colour.a_float() * go.computedAlpha()));
   colour_filer_->Fill(go, colour);
 
   if (tree) {

src/Systems/SDL/SDLColourFilter.cpp
@@ -79,9 +79,8 @@ void SDLColourFilter::Fill(const GraphicsObject& go,
     // Set up shader
     glUseProgramObjectARB(Shaders::getObjectProgram());
     glUniform1iARB(Shaders::getObjectUniformImage(), 0);
-    // The alpha is 255 because we are replacing the image on the screen
-    // instead of blending with it.
-    glUniform1fARB(Shaders::getObjectUniformAlpha(), 1.0f);
+    glUniform1fARB(Shaders::getObjectUniformAlpha(),
+                   (go.computedAlpha() / 255.0f));
     Shaders::loadObjectUniformFromGraphicsObject(go);
 
     float thisx1 = 0;
@@ -89,9 +88,7 @@ void SDLColourFilter::Fill(const GraphicsObject& go,
     float thisx2 = float(screen_rect_.width()) / texture_width_;
     float thisy2 = float(screen_rect_.height()) / texture_height_;
 
-    // Don't blend with the background. Blit on top of it.
-    glDisable(GL_BLEND);
-
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glBegin(GL_QUADS);
     {
       glTexCoord2f(thisx1, thisy2);
@@ -105,8 +102,8 @@ void SDLColourFilter::Fill(const GraphicsObject& go,
       glVertex2f(screen_rect_.x() + screen_rect_.width(), screen_rect_.y());
     }
     glEnd();
+    glBlendFunc(GL_ONE, GL_ZERO);
 
-    glEnable(GL_BLEND);
     glUseProgramObjectARB(0);
   }
 }

src/Systems/SDL/SDLGraphicsSystem.cpp
@@ -86,7 +86,7 @@ void SDLGraphicsSystem::setCursor(int cursor) {
 }
 
 void SDLGraphicsSystem::beginFrame() {
-  glClearColor(0, 0, 0, 0);
+  glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   DebugShowGLErrors();
 
@@ -320,7 +320,7 @@ void SDLGraphicsSystem::setupVideo() {
   glShadeModel(GL_SMOOTH);
 
   // Set the background black
-  glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+  glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
   // Depth buffer setup
   glClearDepth(1.0f);