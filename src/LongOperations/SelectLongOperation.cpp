eglaysher/rlvm

commit 1569933367b12113dc76a3a23456299f8743a9b4

Play sound effects during selections.
There doesn't appear to be anything in the Gameexe.ini that lets the
scripter control this; I suspect that SE#0/1 are hard coded into
the system.

Closes GH-18

src/LongOperations/SelectLongOperation.cpp
@@ -39,6 +39,7 @@
 #include "Systems/Base/EventSystem.hpp"
 #include "Systems/Base/GraphicsSystem.hpp"
 #include "Systems/Base/Renderable.hpp"
+#include "Systems/Base/SoundSystem.hpp"
 #include "Systems/Base/System.hpp"
 #include "Systems/Base/TextSystem.hpp"
 #include "Systems/Base/TextWindow.hpp"
@@ -105,6 +106,7 @@ SelectLongOperation::SelectLongOperation(RLMachine& machine,
 }
 
 void SelectLongOperation::selected(int num) {
+  machine_.system().sound().playSe(1);
   machine_.system().takeSelectionSnapshot(machine_);
   return_value_ = num;
 }
@@ -328,6 +330,10 @@ ButtonSelectLongOperation::~ButtonSelectLongOperation() {
 void ButtonSelectLongOperation::mouseMotion(const Point& p) {
   for (size_t i = 0; i < buttons_.size(); i++) {
     if (buttons_[i].bounding_rect.contains(p)) {
+      if (highlighted_item_ != i) {
+        machine_.system().sound().playSe(0);
+      }
+
       highlighted_item_ = i;
       return;
     }

src/Systems/Base/SelectionElement.cpp
@@ -28,8 +28,10 @@
 #include "Systems/Base/SelectionElement.hpp"
 
 #include "MachineBase/RLMachine.hpp"
-#include "Systems/Base/Surface.hpp"
 #include "Systems/Base/GraphicsSystem.hpp"
+#include "Systems/Base/SoundSystem.hpp"
+#include "Systems/Base/Surface.hpp"
+#include "Systems/Base/System.hpp"
 #include "Systems/Base/Rect.hpp"
 
 using boost::shared_ptr;
@@ -38,7 +40,7 @@ using boost::shared_ptr;
 // SelectionElement
 // -----------------------------------------------------------------------
 SelectionElement::SelectionElement(
-    GraphicsSystem& gs,
+    System& system,
     const boost::shared_ptr<Surface>& normal_image,
     const boost::shared_ptr<Surface>& highlighted_image,
     const boost::function<void(int)>& selection_callback,
@@ -47,7 +49,7 @@ SelectionElement::SelectionElement(
       normal_image_(normal_image),
       highlighted_image_(highlighted_image),
       selection_callback_(selection_callback),
-      graphics_system_(gs) {
+      system_(system) {
 }
 
 SelectionElement::~SelectionElement() {
@@ -66,8 +68,11 @@ void SelectionElement::setMousePosition(const Point& pos) {
   bool start_value = is_highlighted_;
   is_highlighted_ = isHighlighted(pos);
 
-  if (start_value != is_highlighted_)
-    graphics_system_.markScreenAsDirty(GUT_TEXTSYS);
+  if (start_value != is_highlighted_) {
+    system_.graphics().markScreenAsDirty(GUT_TEXTSYS);
+    if (is_highlighted_)
+      system_.sound().playSe(0);
+  }
 }
 
 bool SelectionElement::handleMouseClick(const Point& pos, bool pressed) {

src/Systems/Base/SelectionElement.hpp
@@ -33,13 +33,13 @@
 
 #include "Systems/Base/Rect.hpp"
 
-class GraphicsSystem;
 class Surface;
+class System;
 
 // Represents a clickable element inside TextWindows.
 class SelectionElement {
  public:
-  SelectionElement(GraphicsSystem& gs,
+  SelectionElement(System& system,
                    const boost::shared_ptr<Surface>& normal_image,
                    const boost::shared_ptr<Surface>& highlighted_image,
                    const boost::function<void(int)>& selection_callback,
@@ -67,7 +67,7 @@ class SelectionElement {
   // Callback function for when item is selected.
   boost::function<void(int)> selection_callback_;
 
-  GraphicsSystem& graphics_system_;
+  System& system_;
 
   bool isHighlighted(const Point& p);
 };

src/Systems/SDL/SDLTextWindow.cpp
@@ -105,7 +105,7 @@ void SDLTextWindow::addSelectionItem(const std::string& utf8str,
                    Size(text_insertion_point_x_, text_insertion_point_y_);
 
   SelectionElement* element = new SelectionElement(
-      system().graphics(),
+      system(),
       shared_ptr<Surface>(new SDLSurface(getSDLGraphics(system()), normal)),
       shared_ptr<Surface>(new SDLSurface(getSDLGraphics(system()), inverted)),
       selectionCallback(), selection_id, position);

commit 105b3a02cd298caa2022509fb0f849d7ecf2e201

Don't crash on selections in Kanon.
This checks to see if there's a valid sound effect in slot 0 or 1 before
playing a sound effect during a selection. Fixes regression introduced in
1569933367b12113dc76a3a23456299f8743a9b4.

src/LongOperations/SelectLongOperation.cpp
@@ -106,7 +106,8 @@ SelectLongOperation::SelectLongOperation(RLMachine& machine,
 }
 
 void SelectLongOperation::selected(int num) {
-  machine_.system().sound().playSe(1);
+  if (machine_.system().sound().hasSe(1))
+    machine_.system().sound().playSe(1);
   machine_.system().takeSelectionSnapshot(machine_);
   return_value_ = num;
 }
@@ -330,7 +331,8 @@ ButtonSelectLongOperation::~ButtonSelectLongOperation() {
 void ButtonSelectLongOperation::mouseMotion(const Point& p) {
   for (size_t i = 0; i < buttons_.size(); i++) {
     if (buttons_[i].bounding_rect.contains(p)) {
-      if (highlighted_item_ != i) {
+      if (highlighted_item_ != i &&
+          machine_.system().sound().hasSe(0)) {
         machine_.system().sound().playSe(0);
       }
 

src/Systems/Base/SelectionElement.cpp
@@ -70,7 +70,7 @@ void SelectionElement::setMousePosition(const Point& pos) {
 
   if (start_value != is_highlighted_) {
     system_.graphics().markScreenAsDirty(GUT_TEXTSYS);
-    if (is_highlighted_)
+    if (is_highlighted_ && system_.sound().hasSe(0))
       system_.sound().playSe(0);
   }
 }

src/Systems/Base/SoundSystem.hpp
@@ -295,6 +295,9 @@ class SoundSystem {
   // Plays an interface sound effect. |se_num| is an index into the #SE table.
   virtual void playSe(const int se_num) = 0;
 
+  // Returns whether there is a sound effect |se_num| in the table.
+  virtual bool hasSe(const int se_num) = 0;
+
   // ---------------------------------------------------------------------
 
   // Koe (voice) functions

src/Systems/SDL/SDLSoundSystem.cpp
@@ -292,6 +292,11 @@ void SDLSoundSystem::playSe(const int se_num) {
   }
 }
 
+bool SDLSoundSystem::hasSe(const int se_num) {
+  SeTable::const_iterator it = seTable().find(se_num);
+  return it != seTable().end();
+}
+
 int SDLSoundSystem::bgmStatus() const {
   boost::shared_ptr<SDLMusic> currently_playing = SDLMusic::CurrnetlyPlaying();
   if (currently_playing) {

src/Systems/SDL/SDLSoundSystem.hpp
@@ -62,6 +62,7 @@ class SDLSoundSystem : public SoundSystem {
   virtual void wavFadeOut(const int channel, const int fadetime);
 
   virtual void playSe(const int se_num);
+  virtual bool hasSe(const int se_num);
 
   virtual int bgmStatus() const;
   virtual void bgmPlay(const std::string& bgm_name, bool loop);

test/TestSystem/TestSoundSystem.cpp
@@ -105,6 +105,10 @@ void TestSoundSystem::wavFadeOut(const int channel, const int fadetime) {
 void TestSoundSystem::playSe(const int se_num) {
 }
 
+bool TestSoundSystem::hasSe(const int se_num) {
+  return false;
+}
+
 bool TestSoundSystem::koePlaying() const {
   return false;
 }

test/TestSystem/TestSoundSystem.hpp
@@ -64,6 +64,7 @@ class TestSoundSystem : public SoundSystem {
   virtual void wavFadeOut(const int channel, const int fadetime);
 
   virtual void playSe(const int se_num);
+  virtual bool hasSe(const int se_num);
 
   virtual bool koePlaying() const;
   virtual void koeStop();