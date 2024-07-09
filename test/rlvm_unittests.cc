f094a40c262475a9916f81f1e29ebff8c033cb1f
Stanislau Tsitsianok <tsmx@tut.by>
2024-07-09 13:37:31 +0300

    Initialize GMock in unittests.
    Allows using GMock command line options.

--- test/rlvm_unittests.cc
+++ test/rlvm_unittests.cc
@@ -28,8 +28,10 @@
 #include <iostream>
 
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
 int main(int argc, char** argv) {
   testing::InitGoogleTest(&argc, argv);
+  testing::InitGoogleMock(&argc, argv);
   return RUN_ALL_TESTS();
 }
